0c5cb23112e732ae50cf2006dc09d35eacdd1253
Robin Holzinger <rnholzinger01@gmail.com>
2023-01-18 21:58:34 +0100

    traefik reverse proxy + refactoring (#8)
    * Refactoring & traefik reverse proxy
    
    * Switched to Python3.10
    
    * removed sub repo
    
    * Dockerized node auth frontend
    
    * Readme improvements
    
    * makefile and docker-compose adjustments

--- api/database/setup.py
+++ api/database/setup.py
@@ -15,7 +15,7 @@ async def setup_db_client(running_app: FastAPI):
     # TODO: reuse CONFIG from config.py
     # logger.info("Setting up database connection")
     running_app.state.mongodb_client = AsyncIOMotorClient(
-        "mongodb://admin:password@localhost:27017/"
+        "mongodb://admin:password@mongo.db:27017/"
     )
     await init_beanie(
         database=running_app.state.mongodb_client["tumai-space"],
