6c917811ad83d2324b35b6e7daa6289266c84b6d
MunzerDw <munzerdwedari@gmail.com>
2023-03-18 11:16:35 +0100

    fix mobile height

--- web-app/pages/_app.js
+++ web-app/pages/_app.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import SuperTokensReact, { SuperTokensWrapper } from 'supertokens-auth-react';
 import * as SuperTokensConfig from '../config/frontendConfig';
 import { RootStoreProvider } from '../providers/RootStoreProvider';
@@ -7,6 +8,12 @@ if (typeof window !== 'undefined') {
 	SuperTokensReact.init(SuperTokensConfig.frontendConfig());
 }
 function App({ Component, pageProps }) {
+	useEffect(() => {
+		document.documentElement.style.setProperty(
+			'--vh',
+			window.innerHeight * 0.01 + 'px'
+		);
+	}, []);
 	return (
 		<SuperTokensWrapper>
 			<RootStoreProvider hydrationData={pageProps.hydrationData}>
