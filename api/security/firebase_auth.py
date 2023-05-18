da94e12072d70fee8bb497cf09397f78d56008b7
Robin Holzinger <nerotycs@gmail.com>
2023-05-18 12:30:57 +0200

    add firebase_auth

---
+++ api/security/firebase_auth.py
@@ -0,0 +1,17 @@
+
+from pathlib import Path
+
+import firebase_admin
+from firebase_admin import credentials, auth
+
+
+ROOT = Path(__file__)
+
+def init_firebase_auth():
+    credentials = credentials.Certificate(ROOT.parent / ".secrets" / "tumai-space-firebase-adminsdk-8bc08-452ccb2773.json")
+    app = firebase_admin.initialize_app(credentials)
+
+
+def verify_id_token(jwt):
+   user = auth.verify_id_token(jwt)
+   return user
