37adbb5553a2d21c9248ce0e410afe667cedd285
leonvol <31935319+leonvol@users.noreply.github.com>
2023-05-19 19:22:58 +0200

    Reformat failing files using black

--- api/security/firebase_auth.py
+++ api/security/firebase_auth.py
@@ -1,4 +1,3 @@
-
 from pathlib import Path
 
 import firebase_admin
@@ -7,11 +6,14 @@ from firebase_admin import credentials, auth
 
 ROOT = Path(__file__)
 
+
 def init_firebase_auth():
-    credentials = credentials.Certificate(ROOT.parent / ".secrets" / "tumai-space-firebase-adminsdk-8bc08-452ccb2773.json")
+    credentials = credentials.Certificate(
+        ROOT.parent / ".secrets" / "tumai-space-firebase-adminsdk-8bc08-452ccb2773.json"
+    )
     app = firebase_admin.initialize_app(credentials)
 
 
 def verify_id_token(jwt):
-   user = auth.verify_id_token(jwt)
-   return user
+    user = auth.verify_id_token(jwt)
+    return user
