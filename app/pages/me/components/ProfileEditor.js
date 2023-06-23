7f28938e2d5b1f33e3258a44b57100e6385383cd
Florian Klein <florianbe.klein@gmail.com>
2023-06-23 10:57:55 +0200

    reworked profie editor, job experience can now be added

--- app/pages/me/components/ProfileEditor.js
+++ app/pages/me/components/ProfileEditor.js
@@ -1,131 +1,247 @@
-import { observer } from 'mobx-react';
-import Input from '/components/Input';
-import Textarea from '/components/Textarea';
-import { useStores } from '/providers/StoreProvider';
+import { observer } from "mobx-react";
+import Input from "/components/Input";
+import Textarea from "/components/Textarea";
+import { useStores } from "/providers/StoreProvider";
+import { useRouter } from "next/router";
 
-function ProfileEditor() {
-	const { uiModel, meModel } = useStores();
-	const editorProfile = meModel.editorProfile;
+function ProfileEditor({ isSignUpForm = false }) {
+  const { uiModel, meModel } = useStores();
+  const editorProfile = meModel.editorProfile;
+  const router = useRouter();
 
-	function handleChange(e) {
-		meModel.updateEditorProfile({
-			[e.target.name]: e.target.value,
-		});
-	}
+  function handleChange(e) {
+    meModel.updateEditorProfile({
+      [e.target.name]: e.target.value,
+    });
+  }
 
-	return (
-		<div className='flex flex-col space-y-6 rounded-lg p-6 bg-white dark:bg-gray-700 w-full'>
-			<div className='text-2xl font-light'>Edit profile</div>
-			<form
-				onSubmit={async (e) => {
-					e.preventDefault();
-					await meModel.editProfile();
-					meModel.getProfile();
-					uiModel.toggleModal();
-				}}
-				className='flex flex-col gap-4 lg:grid lg:grid-cols-2 lg:space-y-0 lg:gap-8'
-			>
-				<Input
-					label='First name'
-					type='text'
-					id='first_name'
-					name='first_name'
-					value={editorProfile.first_name}
-					onChange={handleChange}
-					required={true}
-				/>
-				<Input
-					label='Last name'
-					type='text'
-					id='last_name'
-					name='last_name'
-					value={editorProfile.last_name}
-					onChange={handleChange}
-					required={true}
-				/>
-				<Input
-					label='Nationality'
-					type='text'
-					id='nationality'
-					name='nationality'
-					value={editorProfile.nationality}
-					onChange={handleChange}
-					required={true}
-				/>
-				<Input
-					label='University'
-					type='text'
-					id='university'
-					name='university'
-					value={editorProfile.university}
-					onChange={handleChange}
-					required={true}
-				/>
-				<Input
-					label='Degree level'
-					type='text'
-					id='degree_level'
-					name='degree_level'
-					value={editorProfile.degree_level}
-					onChange={handleChange}
-					required={true}
-				/>
-				<Input
-					label='Degree name'
-					type='text'
-					id='degree_name'
-					name='degree_name'
-					value={editorProfile.degree_name}
-					onChange={handleChange}
-					required={true}
-				/>
-				<Input
-					label='Semester'
-					type='number'
-					id='degree_semester'
-					name='degree_semester'
-					value={editorProfile.degree_semester}
-					onChange={handleChange}
-					required={true}
-				/>
-				<Input
-					label='Current job'
-					type='text'
-					id='currentJob'
-					name='currentJob'
-					value={editorProfile.currentJob}
-					onChange={handleChange}
-				/>
-				<div className='col-span-2'>
-					<Textarea
-						label='Description'
-						type='text'
-						id='description'
-						name='description'
-						value={editorProfile.description}
-						onChange={handleChange}
-						required={false}
-					/>
-				</div>
-				<div className='col-span-2 flex space-x-2'>
-					<button
-						type='submit'
-						className='p-4 px-8 py-1 rounded-lg w-1/2 bg-gray-200 text-black'
-					>
-						<div>save</div>
-					</button>
-					<button
-						onClick={() => {
-							uiModel.toggleModal();
-						}}
-						className='p-4 px-8 py-1 rounded-lg w-1/2 border-2'
-					>
-						<div>cancel</div>
-					</button>
-				</div>
-			</form>
-		</div>
-	);
+  function handleListItemChange(e, index, type) {
+    const { name, value } = e.target;
+    if (type === "job_history") {
+      meModel.updateEditorProfile({
+        job_history: editorProfile.job_history.map((item, i) => {
+          if (i === index) {
+            return {
+              ...item,
+              [name]: value,
+            };
+          }
+          return item;
+        }),
+      });
+    } else if (type === "social_networks") {
+      meModel.updateEditorProfile({
+        social_networks: editorProfile.social_networks.map((item, i) => {
+          if (i === index) {
+            return {
+              ...item,
+              [name]: value,
+            };
+          }
+          return item;
+        }),
+      });
+    }
+  }
+
+  function handleAddExperience() {
+    const newExperience = {
+      employer: "",
+      position: "",
+      date_from: "",
+      date_to: "",
+    };
+    if (!editorProfile.job_history) {
+      meModel.updateEditorProfile({
+        job_history: [newExperience],
+      });
+      return;
+    }
+    // if job history exists, add new experience to the end of the array
+    meModel.updateEditorProfile({
+      job_history: [...editorProfile.job_history, newExperience],
+    });
+  }
+
+  function handleRemoveExperience(index) {
+    const updatedExperience = [...editorProfile.job_history];
+    updatedExperience.splice(index, 1);
+    meModel.updateEditorProfile({
+      job_history: updatedExperience,
+    });
+  }
+
+  return (
+    <div className="flex flex-col space-y-6 rounded-lg p-6 bg-white dark:bg-gray-700 w-full">
+      <div className="text-2xl font-light">Edit profile</div>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          await meModel.editProfile();
+          meModel.getProfile();
+          isSignUpForm ? router.push("/") : uiModel.toggleModal();
+        }}
+        className="flex flex-col gap-4 lg:grid lg:grid-cols-2 lg:space-y-0 lg:gap-8"
+      >
+        <Input
+          label="First name"
+          type="text"
+          id="first_name"
+          name="first_name"
+          value={editorProfile.first_name}
+          onChange={handleChange}
+          required={true}
+        />
+        <Input
+          label="Last name"
+          type="text"
+          id="last_name"
+          name="last_name"
+          value={editorProfile.last_name}
+          onChange={handleChange}
+          required={true}
+        />
+        <Input
+          label="Nationality"
+          type="text"
+          id="nationality"
+          name="nationality"
+          value={editorProfile.nationality}
+          onChange={handleChange}
+          required={true}
+        />
+        <Input
+          label="University"
+          type="text"
+          id="university"
+          name="university"
+          value={editorProfile.university}
+          onChange={handleChange}
+          required={true}
+        />
+        <Input
+          label="Degree level"
+          type="text"
+          id="degree_level"
+          name="degree_level"
+          value={editorProfile.degree_level}
+          onChange={handleChange}
+          required={true}
+        />
+        <Input
+          label="Degree name"
+          type="text"
+          id="degree_name"
+          name="degree_name"
+          value={editorProfile.degree_name}
+          onChange={handleChange}
+          required={true}
+        />
+        <Input
+          label="Semester"
+          type="number"
+          id="degree_semester"
+          name="degree_semester"
+          value={editorProfile.degree_semester}
+          onChange={handleChange}
+          required={true}
+        />
+        <Input
+          label="Current job"
+          type="text"
+          id="currentJob"
+          name="currentJob"
+          value={editorProfile.currentJob}
+          onChange={handleChange}
+        />
+        <div className="col-span-2">
+          <Textarea
+            label="Description"
+            type="text"
+            id="description"
+            name="description"
+            value={editorProfile.description}
+            onChange={handleChange}
+            required={false}
+          />
+        </div>
+        <div className="col-span-2 text-xl font-light">Job history</div>
+        <div className="col-span-2 text-black font-light">
+          You can update your job history and add your previous work experience
+          here.
+        </div>
+        {!editorProfile.job_history
+          ? null
+          : editorProfile.job_history.map((experience, index) => (
+              <div key={index} className="grid grid-cols-2 gap-4">
+                <Input
+                  label="Employer"
+                  type="text"
+                  name="employer"
+                  value={experience.Employer}
+                  required={true}
+                  onChange={(e) =>
+                    handleListItemChange(e, index, "job_history")
+                  }
+                />
+                <Input
+                  label="Position"
+                  type="text"
+                  name="position"
+                  required={true}
+                  value={experience.Position}
+                  onChange={(e) =>
+                    handleListItemChange(e, index, "job_history")
+                  }
+                />
+                <Input
+                  label="Start date"
+                  type="date"
+                  name="date_from"
+                  required={true}
+                  value={experience.Startdate}
+                  onChange={(e) =>
+                    handleListItemChange(e, index, "job_history")
+                  }
+                />
+                <Input
+                  label="End date"
+                  type="date"
+                  name="date_to"
+                  required={true}
+                  value={experience.Enddate}
+                  onChange={(e) =>
+                    handleListItemChange(e, index, "job_history")
+                  }
+                />
+                <button onClick={() => handleRemoveExperience(index)}>
+                  Remove
+                </button>
+              </div>
+            ))}
+        <button onClick={handleAddExperience}>Add Work Experience</button>
+        <div className="col-span-2 flex space-x-2">
+          <button
+            type="submit"
+            className="p-4 px-8 py-1 rounded-lg w-1/2 bg-gray-200 text-black"
+          >
+            <div>save</div>
+          </button>
+          {isSignUpForm ? null : (
+            <button
+              onClick={() => {
+                // if user currently signs up, redirect him to main page
+                uiModel.toggleModal();
+              }}
+              className="p-4 px-8 py-1 rounded-lg w-1/2 border-2"
+            >
+              <div>cancel</div>
+            </button>
+          )}
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default observer(ProfileEditor);
--- app/pages/members/signup-form.js
+++ app/pages/members/signup-form.js
@@ -1,169 +1,18 @@
-"use client";
-import React, { useState } from "react";
-import axios from "axios";
-import { useRouter } from "next/navigation";
-import { useAuth, getUserAuthToken } from "../../providers/AuthContextProvider";
-import Banner from "../../components/Banner";
-import Page from "../../components/Page";
 import ProfileEditor from "../me/components/ProfileEditor";
-
-// ========================================
-// SIGN UP FORM
-// - Form for user to fill in their profile information
-// - send information to backend endpoint ./localhost:8000/profile/{user_id}
-// - can be accessed via localhost:8000/signup-form
-// ========================================
+import Page from "../../components/Page";
+import { observer } from "mobx-react";
+import { useStores } from "/providers/StoreProvider";
 
 function SignUpForm() {
-  // authentication information used for updating the user profile in the database
-  // const { user } = useAuth();
-  const router = useRouter();
-  const [error, setError] = useState(false);
-  const [userID, setUserID] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+  const { meModel } = useStores();
+  const user = meModel.user;
+  const profile = user?.profile;
 
-  // form data, as required by backend endpoint
-  const [formData, setFormData] = useState({
-    email: "",
-    phone: "",
-    first_name: "",
-    last_name: "",
-    birthday: "",
-    nationality: "",
-    description: "",
-    activity_status: "",
-    degree_level: "",
-    degree_name: "",
-    degree_semester: "",
-    university: "",
-    job_history: [],
-    social_networks: [],
-  });
-
-  // handle change of form data, update state and corresponding form data values
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  // handle change of array fields, update state and corresponding form data values
-  const handleArrayFieldChange = (key, index, subKey) => (e) => {
-    const { name, value } = e.target;
-    setFormData((prevState) => ({
-      ...prevState,
-      [key]: prevState[key].map((item, i) => {
-        if (i === index) {
-          return {
-            ...item,
-            [subKey]: value,
-          };
-        }
-        return item;
-      }),
-    }));
-  };
-
-  // add a new array field to form data (either job history or social network)
-  const addArrayField = (key) => () => {
-    setFormData((prevState) => ({
-      ...prevState,
-      [key]: [
-        ...prevState[key],
-        {
-          // if the key is job_history, add a new job history item, else add a new social network item
-          ...(key === "job_history"
-            ? {
-                employer: "",
-                position: "",
-                date_from: "",
-                date_to: "",
-              }
-            : {
-                type: "",
-                handle: "",
-                link: "",
-              }),
-        },
-      ],
-    }));
-  };
-
-  const removeArrayField = (key) => () => {
-    // remove last entry of the array located at formData[] if there is more than one left
-    setFormData((prevState) => ({
-      ...prevState,
-      [key]: prevState[key].slice(0, -1),
-    }));
-  };
-
-  async function handleSubmit(event) {
-    event.preventDefault();
-
-    // Check required fields
-    const requiredFields = [
-      "first_name",
-      "last_name",
-      "birthday",
-      "degree_name",
-      "degree_semester",
-    ];
-    const missingFields = requiredFields.filter((field) => {
-      if (Array.isArray(formData[field])) {
-        return formData[field].some((item) =>
-          Object.values(item).some((value) => !value)
-        );
-      }
-      return !formData[field];
-    });
-
-    if (missingFields.length > 0) {
-      setError(true);
-      setErrorMessage(
-        `Please fill in all required fields: ${missingFields.join(", ")}`
-      );
-      return;
-    }
-
-    // Create the payload for the PATCH request
-    const payload = {
-      ...formData,
-      job_history: formData.job_history
-        .map((item) => ({ ...item }))
-        .filter((item) => Object.values(item).some((value) => !!value)),
-      social_networks: formData.social_networks
-        .map((item) => ({ ...item }))
-        .filter((item) => Object.values(item).some((value) => !!value)),
-      time_joined: new Date().toISOString(),
-    };
-
-    console.log(JSON.stringify(payload));
-
-    const authToken = await getUserAuthToken();
-
-    // Send the PATCH request to the backend
-    await axios
-      .patch("http://localhost:8000/me", payload, {
-        headers: {
-          Authorization: authToken,
-        },
-      })
-      .then((response) => {
-        console.log("Profile updated successfully");
-        console.log(payload);
-        // Redirect to main page after updating the profile
-        router.push("/");
-      })
-      .catch((error) => {
-        console.error("Error updating profile:", error);
-        console.log(payload);
-        console.log(formData);
-      });
-    console.log("Submitted form data:", payload);
+  if (!profile) {
+    return <div>Profile not found.</div>;
   }
 
+  meModel.editorProfile = { ...profile };
   return (
     <Page>
       <div className="font-thin text-6xl">Welcome to TUM.ai Space!</div>
@@ -171,94 +20,9 @@ function SignUpForm() {
         Please complete your profile to get started. You can always edit your
         profile later.
       </div>
-      <ProfileEditor />
-      <div className="py-8 px-4">
-        {error && <Banner headline="Error" text={errorMessage} />}
-        <div className="max-w-md mx-auto">
-          <h1 className="text-2xl font-semibold text-gray-850">
-            Welcome to TUM.ai Space!
-          </h1>
-          <form onSubmit={handleSubmit}>
-            {Object.entries(formData).map(([key, value]) => (
-              <div key={key} className="mb-4">
-                <label className="block mb-1 text-sm font-medium text-gray-700">
-                  {key.charAt(0).toUpperCase() + key.slice(1)}:
-                </label>
-                {key === "job_history" || key === "social_networks" ? (
-                  <>
-                    {value.map((item, index) => (
-                      <div key={index}>
-                        {Object.entries(item).map(([subKey, subValue]) => (
-                          <input
-                            key={subKey}
-                            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
-                            type="text"
-                            name={`${key}[${index}].${subKey}`}
-                            value={subValue || ""}
-                            onChange={handleArrayFieldChange(
-                              key,
-                              index,
-                              subKey
-                            )}
-                            placeholder={
-                              subKey.charAt(0).toUpperCase() + subKey.slice(1)
-                            }
-                            required={subKey === "type"}
-                          />
-                        ))}
-                      </div>
-                    ))}
-                    <button
-                      className="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-600"
-                      type="button"
-                      onClick={addArrayField(key)}
-                    >
-                      Add {key.charAt(0).toUpperCase() + key.slice(1)}
-                    </button>
-                    <button
-                      className="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-600"
-                      type="button"
-                      onClick={removeArrayField(key)}
-                    >
-                      Remove {key.charAt(0).toUpperCase() + key.slice(1)}
-                    </button>
-                  </>
-                ) : (
-                  <input
-                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
-                    type={
-                      key === "birthday"
-                        ? "date"
-                        : key === "timeJoined"
-                        ? "datetime-local"
-                        : "text"
-                    }
-                    name={key}
-                    // if key is email, dont allow typing, use the email from the iuser object
-                    value={key === "email" ? "usermailtodo" : value || ""}
-                    onChange={handleChange}
-                    placeholder={key.charAt(0).toUpperCase() + key.slice(1)}
-                    required={
-                      key === "id" ||
-                      key === "firebaseUid" ||
-                      key === "firstName" ||
-                      key === "lastName"
-                    }
-                  />
-                )}{" "}
-              </div>
-            ))}{" "}
-            <button
-              className="w-full py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500"
-              type="submit"
-            >
-              Submit Profile
-            </button>{" "}
-          </form>
-        </div>
-      </div>
+      <ProfileEditor isSignUpForm={true} />
     </Page>
   );
 }
 
-export default SignUpForm;
+export default observer(SignUpForm);
