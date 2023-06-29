1cb4d39b56e4e72adc1f8db3fd69a1fffad99286
nilsrehtanz <62997020+nilsrehtanz@users.noreply.github.com>
2023-06-29 23:12:52 +0200

    [api/membership_application] added membership application referral route (#88)
    * [api/membership_application] added membership application referral route
    
    * [membership_application] added email field and fixed Munzers comments
    
    * removed user_id and added refferal_by as primary key
    
    * log access token for dev
    
    ---------
    
    Co-authored-by: Nils Rehtanz <ge35xex@mytum.dege35xex@mytum.de>
    Co-authored-by: MunzerDw <munzerdwedari@gmail.com>

--- api/membership_application/routes.py
+++ api/membership_application/routes.py
@@ -1,26 +1,32 @@
 from typing import (
+    Annotated,
     List,
     Union,
 )
 
 from fastapi import (
     APIRouter,
+    Body,
     Request,
 )
 
 from database.membership_application_connector import (
+    create_db_membership_application_referral,
     list_db_membership_application,
     retrieve_db_membership_application,
 )
 from membership_application.api_models import (
     MembershipApplicationListOut,
     MembershipApplicationOut,
+    MembershipApplicationReferralIn,
 )
 from membership_application.response_models import (
     ResponseMembershipApplication,
     ResponseMembershipApplicationList,
+    ResponseSubmitMembershipApplicationReferral,
 )
 from security.decorators import (
+    ensure_authenticated,
     ensure_authorization,
 )
 from utils.error_handlers import (
@@ -79,4 +85,26 @@ def get_membership_application(request: Request, application_id: int) -> dict:
         "response_type": "success",
         "description": "Retrieved application successfully",
         "data": MembershipApplicationOut.from_db_model(db_model),
+    }
+
+
+@router.post(
+    "/membership_application/submit/referral",
+    response_description="Refer a new member.",
+    response_model=ResponseSubmitMembershipApplicationReferral,
+)
+@error_handlers
+@ensure_authenticated
+def submit_membership_application_referral(
+    request: Request,
+    data: Annotated[MembershipApplicationReferralIn, Body(embed=True)],
+) -> dict:
+    create_db_membership_application_referral(
+        request.app.state.sql_engine, request.state.profile.id, data
+    )
+
+    return {
+        "status_code": 200,
+        "response_type": "success",
+        "description": "Referral submitted successfully",
     }
