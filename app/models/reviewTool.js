01beade6e0e7d76f4b7c9ada4e408d70937f4ab2
MunzerDw <munzerdwedari@gmail.com>
2023-07-04 11:11:03 +0300

    filter applications

--- app/models/reviewTool.js
+++ app/models/reviewTool.js
@@ -3,6 +3,8 @@ import { makeAutoObservable } from 'mobx';
 export class ReviewToolModel {
 	root;
 	applications = [];
+	filteredApplications = [];
+	search = '';
 
 	constructor(root) {
 		this.root = root;
@@ -11,8 +13,25 @@ export class ReviewToolModel {
 		this.fetchApplications();
 	}
 
+	setSearch(value) {
+		this.search = value;
+		this.filterApplications();
+	}
+
+	filterApplications() {
+		this.filteredApplications = this.applications.filter((application) => {
+			return (
+				!this.search ||
+				JSON.stringify({ ...application, _id: '' })
+					.toLocaleLowerCase()
+					.includes(this.search.toLocaleLowerCase())
+			);
+		});
+	}
+
 	async fetchApplications() {
 		const applications = await this.root.GET('/membership_applications/');
 		this.applications = applications;
+		this.filteredApplications = applications;
 	}
 }
--- app/pages/review-tool.js
+++ app/pages/review-tool.js
@@ -1,18 +1,74 @@
 import { observer } from 'mobx-react';
+import Link from 'next/link';
 import ProtectedItem from '../components/ProtectedItem';
+import Tabs from '../components/Tabs';
+import Icon from '/components/Icon';
 import Page from '/components/Page';
 import { useStores } from '/providers/StoreProvider';
 
 function ReviewTool() {
-	const { reviewToolModel } = useStores();
-
 	// TODO choose member's name from member profiles directly and fill in information accordingly
 	return (
 		<ProtectedItem showNotFound roles={['submit_reviews']}>
-			<Page>awdaw</Page>
+			<Page>
+				<Tabs
+					tabs={{
+						Applications: <Applications />,
+						Review: <Review />,
+					}}
+				/>
+			</Page>
 		</ProtectedItem>
 		// TODO download button for cert
 	);
 }
 
-export default observer(ReviewTool);
+const Applications = observer(() => {
+	const { reviewToolModel } = useStores();
+	return (
+		<div className='flex flex-col space-y-4 pt-4'>
+			<div className='m-auto bg-white dark:bg-gray-700 rounded w-full lg:w-1/2 p-2 flex space-x-4'>
+				<Icon name={'FaSearch'} className='p-2 rounded' />
+				<input
+					value={reviewToolModel.search}
+					onChange={(e) => {
+						reviewToolModel.setSearch(e.target.value);
+					}}
+					placeholder='search..'
+					className='w-full bg-transparent outline-none'
+				></input>
+				{reviewToolModel.search && (
+					<button
+						onClick={(e) => {
+							reviewToolModel.setSearch('');
+						}}
+					>
+						clear
+					</button>
+				)}
+			</div>
+			{reviewToolModel.filteredApplications.map((application) => (
+				<Application data={application} />
+			))}
+		</div>
+	);
+});
+
+function Application({ data }) {
+	return (
+		<div className='bg-white rounded-2xl shadow grid grid-cols-4 lg:grid-cols-6 p-6'>
+			<div>{data.id}</div>
+			<div>{data.first_name}</div>
+			<div>{data.last_name}</div>
+			<div>{data.email}</div>
+			<div>{data.degree_level}</div>
+			<Link href={data.resume}>Resume</Link>
+		</div>
+	);
+}
+
+const Review = observer(() => {
+	return <div>review</div>;
+});
+
+export default ReviewTool;
