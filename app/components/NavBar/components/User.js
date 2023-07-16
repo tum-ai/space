2c3406cd3553cf6d14551ab96d20ac0ab76acee9
Robin Holzinger <nerotycs@gmail.com>
2023-07-16 12:22:59 +0200

    fix: Eliminate linting issues & run linting on `npm ci` (#94)
    * fix: Fix frontend linting
    
    * fix some more linting issues
    
    * Fix build
    
    ---------
    
    Co-authored-by: leonvol <31935319+leonvol@users.noreply.github.com>

--- app/components/NavBar/components/User.js
+++ app/components/NavBar/components/User.js
@@ -2,6 +2,8 @@ import { observer } from 'mobx-react';
 import Link from 'next/link';
 import Icon from '/components/Icon';
 import { useStores } from '/providers/StoreProvider';
+import Image from 'next/image';
+import { FaUser } from 'react-icons/fa';
 
 function User() {
 	const { uiModel, meModel } = useStores();
@@ -23,13 +25,14 @@ function User() {
 						}
 					>
 						{user.profile.profile_picture ? (
-							<img
-								className='rounded-full w-8 h-8 object-cover'
-								src={user.profile.profile_picture}
-							/>
+							<Image
+							className='rounded-full w-8 h-8 object-cover'
+							src={user.profile.profile_picture}
+							alt=''
+						  />
 						) : (
 							<div className='rounded-full w-8 h-8 bg-gray-300 dark:bg-gray-800 flex text-center drop-shadow-lg'>
-								<Icon
+								<FaUser
 									name={'FaUser'}
 									className='m-auto text-xl text-white'
 								/>
