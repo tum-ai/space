06a1d4310b1f290a85e86698fa72240c15e8640c
Aman Varshney <amanvars@gmail.com>
2023-08-16 23:17:12 +0200

    feat: Dev 175 edit view review (#97)

--- api/space_api/review_tool/response_models.py
+++ api/space_api/review_tool/response_models.py
@@ -1,7 +1,23 @@
 from pydantic import ConfigDict
 
+from space_api.review_tool.api_models import (
+    ApplicationReviewOut,
+)
 from space_api.utils.response import BaseResponse
 
 
 class ResponseSubmitReview(BaseResponse):
     model_config = ConfigDict(json_schema_extra={"example": []})
+
+class ResponseApplicationReview(BaseResponse):
+    data: ApplicationReviewOut
+
+    class Config:
+        schema_extra = BaseResponse.schema_wrapper(ApplicationReviewOut.dummy())
+
+
+class ResponseApplicationReviewList(BaseResponse):
+    data: list[ApplicationReviewOut]
+
+    class Config:
+        schema_extra = BaseResponse.schema_wrapper(ApplicationReviewOut.dummy())
