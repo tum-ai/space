52f965d9bab7a148a8c3223f06b187bef3018ffe
MunzerDw <munzerdwedari@gmail.com>
2023-08-16 20:12:52 +0200

    use str instead of EmailStr because its causing error

--- api/space_api/profiles/routes.py
+++ api/space_api/profiles/routes.py
@@ -2,7 +2,6 @@ import datetime
 from typing import Annotated
 
 from fastapi import APIRouter, Body, Request
-from pydantic import EmailStr
 
 from space_api.database.profiles_connector import (
     create_db_department_memberships,
@@ -409,7 +408,7 @@ def invite_members(
         for profile in created_profiles_out:
             link = generate_reset_password_link(profile.email)
             send_email(
-                EmailStr(profile.email),
+               profile.email,
                 "Welcome to TUM.ai space",
                 f"Dear {profile.first_name} {profile.last_name},\n\nYou have been \
                 invited to TUM.ai space. Please follow the link below to reset your \
