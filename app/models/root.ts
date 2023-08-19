a89656d80016ad4685582587841339ad189b0902
Simon Huang <simon.huang@tum.de>
2023-08-19 14:19:24 +0200

    add typescript types

--- app/models/root.ts
+++ app/models/root.ts
@@ -10,6 +10,16 @@ import { RolesModel } from "./roles.js";
 import { UiModel } from "./ui.js";
 
 export class RootModel {
+  profileModel: ProfileModel;
+  profilesModel: ProfilesModel;
+  inviteModel: InviteModel;
+  uiModel: UiModel;
+  meModel: MeModel;
+  rolesModel: RolesModel;
+  certificateModel: CertificateModel;
+  departmentMembershipsModel: DepartmentMembershipsModel;
+  reviewToolModel: ReviewToolModel;
+
   constructor() {
     this.profileModel = new ProfileModel(this);
     this.profilesModel = new ProfilesModel(this);
@@ -22,19 +32,16 @@ export class RootModel {
     this.reviewToolModel = new ReviewToolModel(this);
   }
 
-  async GET(path) {
+  async GET(path: string) {
     try {
       const response = await axios(path);
       return response?.data?.data;
     } catch (error) {
       console.log(error);
-      this.uiModel.updateModalContent(<div>Error:</div>);
-      this.uiModel.toggleModal();
-      return;
     }
   }
 
-  async PATCH(path, data) {
+  async PATCH(path: string, data: any) {
     try {
       const response = await axios(path, {
         data: { data: data },
@@ -43,16 +50,10 @@ export class RootModel {
       return response?.data.data;
     } catch (error) {
       console.log(error);
-      const response = error.response;
-      const data = response.data;
-      const message = data.detail[0]["msg"];
-      this.uiModel.toggleModal();
-      this.uiModel.updateModalContent(<div>Error: {message}</div>);
-      return;
     }
   }
 
-  async DELETE(path, data) {
+  async DELETE(path: string, data: any) {
     try {
       const response = await axios(path, {
         data: data,
@@ -61,16 +62,15 @@ export class RootModel {
       return response?.data.data;
     } catch (error) {
       console.log(error);
-      const response = error.response;
-      const data = response.data;
-      const message = data.detail[0]["msg"];
-      this.uiModel.toggleModal();
-      this.uiModel.updateModalContent(<div>Error: {message}</div>);
-      return;
     }
   }
 
-  async POST(path, data, config, returnResponse) {
+  async POST(
+    path: string,
+    data: any,
+    config?: { [key: string]: any },
+    returnResponse?: boolean,
+  ) {
     try {
       const response = await axios(path, {
         data: { data: data },
@@ -83,12 +83,6 @@ export class RootModel {
       return response?.data;
     } catch (error) {
       console.log(error);
-      const response = error.response;
-      const data = response.data;
-      const message = data.detail[0]["msg"];
-      this.uiModel.toggleModal();
-      this.uiModel.updateModalContent(<div>Error: {message}</div>);
-      return;
     }
   }
 }
