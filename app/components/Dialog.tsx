a3800d6d4ec5845e84814d5859c6d4269460a8ad
MunzerDw <munzerdwedari@gmail.com>
2023-08-25 13:22:46 +0200

    add missing dev dep

--- app/components/Dialog.tsx
+++ app/components/Dialog.tsx
@@ -1,29 +1,29 @@
-import * as DialogRadix from '@radix-ui/react-dialog';
-import { clsx } from 'clsx';
-import { useState } from 'react';
+import * as DialogRadix from "@radix-ui/react-dialog";
+import { clsx } from "clsx";
+import { useState } from "react";
 
 function Dialog({ trigger, children }) {
-	let [isOpen, setIsOpen] = useState(false);
-	return (
-		<DialogRadix.Root open={isOpen} onOpenChange={setIsOpen}>
-			<DialogRadix.Trigger asChild>{trigger}</DialogRadix.Trigger>
-			<DialogRadix.Portal>
-				<DialogRadix.Overlay className='fixed inset-0 z-20 bg-black/50' />
-				<DialogRadix.Content
-					className={clsx(
-						'fixed z-50 overflow-auto',
-						'w-[95vw] max-w-[1000px] rounded-lg p-8 md:w-full',
-						'max-h-[90%]',
-						'top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]',
-						'bg-gray-200 dark:bg-gray-800',
-						'outline-none'
-					)}
-				>
-					{children}
-				</DialogRadix.Content>
-			</DialogRadix.Portal>
-		</DialogRadix.Root>
-	);
+  let [isOpen, setIsOpen] = useState(false);
+  return (
+    <DialogRadix.Root open={isOpen} onOpenChange={setIsOpen}>
+      <DialogRadix.Trigger asChild>{trigger}</DialogRadix.Trigger>
+      <DialogRadix.Portal>
+        <DialogRadix.Overlay className="fixed inset-0 z-20 bg-black/50" />
+        <DialogRadix.Content
+          className={clsx(
+            "fixed z-50 overflow-auto",
+            "w-[95vw] max-w-[1000px] rounded-lg p-8 md:w-full",
+            "max-h-[90%]",
+            "left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%]",
+            "bg-gray-200 dark:bg-gray-800",
+            "outline-none",
+          )}
+        >
+          {children}
+        </DialogRadix.Content>
+      </DialogRadix.Portal>
+    </DialogRadix.Root>
+  );
 }
 
 export default Dialog;
