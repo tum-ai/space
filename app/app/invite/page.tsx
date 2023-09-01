2f36246006888000af09f28741e5c4c3ff4cd3da
Simon Huang <simon.huang@tum.de>
2023-09-02 00:03:17 +0200

    change invite to formik

--- app/app/invite/page.tsx
+++ app/app/invite/page.tsx
@@ -2,66 +2,176 @@
 import { Button } from "@components/Button";
 import ProtectedItem from "@components/ProtectedItem";
 import { Section } from "@components/Section";
-import Textarea from "@components/Textarea";
-import { useStores } from "@providers/StoreProvider";
-import { observer } from "mobx-react";
+import { Cross1Icon, PlusIcon } from "@radix-ui/react-icons";
+import axios from "axios";
+import {
+  ErrorMessage,
+  Field,
+  FieldArray,
+  Form,
+  Formik,
+  FormikValues,
+} from "formik";
+import toast from "react-hot-toast";
 
-const Invite = observer(() => {
-  const { inviteModel } = useStores();
-  const text = inviteModel.text;
-  function handleChange(e) {
-    inviteModel.text = e.target.value;
-  }
+const Invite = () => {
+  const initialValues = {
+    invitees: [
+      {
+        email: "",
+        first_name: "",
+        last_name: "",
+        department_handle: "",
+        department_position: "",
+      },
+    ],
+  };
+
   return (
     <ProtectedItem roles={["invite_members"]}>
       <Section>
-        <div className="text-6xl font-thin">Invite Members</div>
-      </Section>
-      <Section>
-        <form
-          onSubmit={async (e) => {
-            e.preventDefault();
-            await inviteModel.invite();
+        <h1 className="mb-8 text-6xl font-thin">Invite Members</h1>
+
+        <Formik
+          initialValues={initialValues}
+          onSubmit={(values: FormikValues) => {
+            toast.promise(
+              axios.post("/profiles/invite/members", {
+                data: values.invitees,
+              }),
+              {
+                loading: "",
+                success: "",
+                error: "",
+              },
+            );
           }}
-          className="grid grid-cols-1 items-end gap-4 rounded-lg bg-gray-200 p-8 dark:bg-gray-600 lg:gap-8"
         >
-          <h2 className="text-2xl">Create Invitations</h2>
-          <p>
-            Write each invite in one line like as below. After submitting the
-            invitations, the invited emails will receive a welcome email and a
-            link to reset their password. Use the following format to invite
-            members:
-            <br />
-            <br />
-            <p>
-              email, first_name, last_name, department_handle,
-              department_position
-            </p>
-            <br />
-            <p>
-              <b>department_handle:</b> DEV, MARKETING, INDUSTRY, MAKEATHON,
-              COMMUNITY, PNS, LNF, VENTURE, EDUCATION, RND
-            </p>
-            <br />
-            <p>
-              <b>department_position:</b> teamlead,president,member,alumni
-            </p>
-          </p>
-          <Textarea
-            label="New members"
-            placeholder="email@example.com,John,Doe,DEV,teamlead"
-            type="text"
-            id="description"
-            name="description"
-            value={text}
-            onChange={handleChange}
-            required={true}
-          />
-          <Button type="submit">invite</Button>
-        </form>
+          {({ values }) => (
+            <Form>
+              <FieldArray name="invitees">
+                {({ insert, remove, push }) => (
+                  <div className="space-y-4">
+                    {values.invitees.length > 0 &&
+                      values.invitees.map((invitee, index) => (
+                        <div className="flex gap-4" key={index}>
+                          <div className="flex flex-col">
+                            <label htmlFor={`invitees.${index}.email`}>
+                              Email
+                            </label>
+                            <Field
+                              className="p-2"
+                              name={`invitees.${index}.email`}
+                              placeholder="daniel.korth@tum.de"
+                              type="text"
+                            />
+                            <ErrorMessage
+                              name={`invitees.${index}.email`}
+                              component="div"
+                              className="field-error"
+                            />
+                          </div>
+
+                          <div className="flex flex-col">
+                            <label htmlFor={`invitees.${index}.first_name`}>
+                              First name
+                            </label>
+                            <Field
+                              className="p-2"
+                              name={`invitees.${index}.first_name`}
+                              placeholder="Daniel"
+                              type="text"
+                            />
+                            <ErrorMessage
+                              name={`invitees.${index}.first_name`}
+                              component="div"
+                              className="field-error"
+                            />
+                          </div>
+
+                          <div className="flex flex-col">
+                            <label htmlFor={`invitees.${index}.last_name`}>
+                              Last name
+                            </label>
+                            <Field
+                              className="p-2"
+                              name={`invitees.${index}.last_name`}
+                              placeholder="Korth"
+                              type="text"
+                            />
+                            <ErrorMessage
+                              name={`invitees.${index}.last_name`}
+                              component="div"
+                              className="field-error"
+                            />
+                          </div>
+
+                          <div className="flex flex-col">
+                            <label
+                              htmlFor={`invitees.${index}.department_handle`}
+                            >
+                              Department handle
+                            </label>
+                            <Field
+                              className="p-2"
+                              name={`invitees.${index}.department_handle`}
+                              placeholder="Makethon"
+                              type="text"
+                            />
+                            <ErrorMessage
+                              name={`invitees.${index}.department_handle`}
+                              component="div"
+                              className="field-error"
+                            />
+                          </div>
+
+                          <div className="flex flex-col">
+                            <label
+                              htmlFor={`invitees.${index}.department_position`}
+                            >
+                              Department position
+                            </label>
+                            <Field
+                              className="p-2"
+                              name={`invitees.${index}.department_position`}
+                              placeholder="Teamlead"
+                              type="text"
+                            />
+                            <ErrorMessage
+                              name={`invitees.${index}.department_position`}
+                              component="div"
+                              className="field-error"
+                            />
+                          </div>
+
+                          <div className="col">
+                            <button type="button" onClick={() => remove(index)}>
+                              <Cross1Icon className="mt-6 h-6 w-6" />
+                            </button>
+                          </div>
+                        </div>
+                      ))}
+
+                    <div className="flex gap-2">
+                      <Button
+                        type="button"
+                        className="flex items-center gap-2"
+                        onClick={() => push({ name: "", email: "" })}
+                      >
+                        <PlusIcon /> Add Invitation
+                      </Button>
+
+                      <Button type="submit">Invite</Button>
+                    </div>
+                  </div>
+                )}
+              </FieldArray>
+            </Form>
+          )}
+        </Formik>
       </Section>
     </ProtectedItem>
   );
-});
+};
 
 export default Invite;
