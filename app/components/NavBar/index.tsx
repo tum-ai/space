bacfd5edfdce5482923a59566ca624da7f10abfb
Simon Huang <simon.huang@tum.de>
2023-09-02 18:17:42 +0200

    refactor nav bar

--- app/components/NavBar/index.tsx
+++ app/components/NavBar/index.tsx
@@ -1,12 +1,91 @@
-import NavBarDesktop from "./components/NavBarDesktop";
-import NavBarMobile from "./components/NavBarMobile";
+import ProtectedItem from "@components/ProtectedItem";
+import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import User from "./components/User";
+import Logo from "./components/Logo";
+import { Section } from "@components/Section";
 
 function NavBar() {
+  const pathname = usePathname();
+  const links = [
+    {
+      path: "/",
+      text: "Home",
+    },
+    {
+      path: "/profiles",
+      text: "Team",
+    },
+    {
+      path: "/feedback",
+      text: "Feedback",
+      protected: true,
+    },
+    {
+      path: "/invite",
+      text: "Invite Members",
+      protected: true,
+      roles: ["invite_members"],
+    },
+    {
+      path: "/certificate",
+      text: "Certificate",
+      protected: true,
+      roles: ["create_certificate"],
+    },
+    {
+      path: "/review",
+      text: "Review Tool",
+      protected: true,
+      roles: ["submit_reviews"],
+    },
+  ];
   return (
-    <>
-      <NavBarDesktop />
-      <NavBarMobile />
-    </>
+    <NavigationMenu.Root className="relative w-full" orientation="vertical">
+      <Section>
+        <div className="mb-4 flex justify-between">
+          <Logo />
+          <User />
+        </div>
+        <NavigationMenu.List className="flex items-center justify-between overflow-x-auto">
+          <div className="flex gap-6">
+            {links.map((link) => {
+              const component = (
+                <NavigationMenu.Item>
+                  <NavigationMenu.Link asChild>
+                    <Link
+                      key={link["path"]}
+                      href={link.path}
+                      className={
+                        "whitespace-nowrap hover:text-black dark:hover:text-white " +
+                        (pathname.includes(link.path) &&
+                        link.path === "/" &&
+                        pathname == link.path
+                          ? "text-white"
+                          : "text-gray-500")
+                      }
+                    >
+                      {link.text}
+                    </Link>
+                  </NavigationMenu.Link>
+                </NavigationMenu.Item>
+              );
+
+              if (link.protected) {
+                return (
+                  <ProtectedItem key={link["path"]} roles={link.roles}>
+                    {component}
+                  </ProtectedItem>
+                );
+              }
+
+              return component;
+            })}
+          </div>
+        </NavigationMenu.List>
+      </Section>
+    </NavigationMenu.Root>
   );
 }
 
--- app/models/root.ts
+++ app/models/root.ts
@@ -6,13 +6,11 @@ import { ProfileModel } from "./profile";
 import { ProfilesModel } from "./profiles";
 import { ReviewToolModel } from "./reviewTool";
 import { RolesModel } from "./roles";
-import { UiModel } from "./ui";
 
 export class RootModel {
   profileModel: ProfileModel;
   profilesModel: ProfilesModel;
   inviteModel: InviteModel;
-  uiModel: UiModel;
   meModel: MeModel;
   rolesModel: RolesModel;
   certificateModel: CertificateModel;
@@ -23,7 +21,6 @@ export class RootModel {
     this.profileModel = new ProfileModel(this);
     this.profilesModel = new ProfilesModel(this);
     this.inviteModel = new InviteModel(this);
-    this.uiModel = new UiModel(this);
     this.meModel = new MeModel(this);
     this.rolesModel = new RolesModel(this);
     this.certificateModel = new CertificateModel(this);
