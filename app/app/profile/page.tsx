b6083a36b8b69c7f51c3b24a47a7c34e4d9e2c59
Simon Huang <simon.huang@tum.de>
2023-09-02 17:38:24 +0200

    feat: Improve profile pages (#129)

--- app/app/profile/page.tsx
+++ app/app/profile/page.tsx
@@ -1,10 +1,37 @@
+"use client";
+
 import { Section } from "@components/Section";
 import ProfileOverview from "./components/ProfileOverview";
+import { useStores } from "@providers/StoreProvider";
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+import { observer } from "mobx-react";
 
-export default function Profile() {
+const Profile = () => {
+  const { profileModel } = useStores();
+  const searchParams = useSearchParams();
+  const id = searchParams.get("id");
+
+  useEffect(() => {
+    if (!id) return;
+    profileModel.getProfile(typeof id === "string" ? id : id[0]);
+  }, [profileModel, id]);
+
+  const profile = profileModel.profile;
+
+  if (profileModel.loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!profile) {
+    return <div>Profile not found.</div>;
+  }
+
   return (
     <Section>
-      <ProfileOverview />
+      <ProfileOverview profile={profile} />
     </Section>
   );
-}
+};
+
+export default observer(Profile);
