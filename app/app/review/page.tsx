ad22fbbd5e188b87d2162de26f4fdf305da0a5e8
MunzerDw <munzerdwedari@gmail.com>
2023-09-03 23:02:46 +0200

    add venture form

--- app/app/review/page.tsx
+++ app/app/review/page.tsx
@@ -7,6 +7,7 @@ import ProtectedItem from "@components/ProtectedItem";
 import { Section } from "@components/Section";
 import Select from "@components/Select";
 import Tabs from "@components/Tabs";
+import Textarea from "@components/Textarea";
 import Tooltip from "@components/Tooltip";
 import { useStores } from "@providers/StoreProvider";
 import { ErrorMessage, Field, Form, Formik, FormikValues } from "formik";
@@ -201,11 +202,19 @@ class ReviewForms {
   };
 }
 
-function ReviewForm2() {
-  const [formType, setFormType] = useState(undefined);
+function ReviewForm() {
+  const [formType, setFormType] = useState("MEMBERSHIP");
+
+  let reviewFormComponent = <p>No form type selected.</p>;
+  if (formType == "MEMBERSHIP") {
+    reviewFormComponent = <MembershipReviewForm />;
+  }
+  if (formType == "VENTURE") {
+    reviewFormComponent = <VentureReviewForm />;
+  }
 
   return (
-    <div>
+    <div className="flex flex-col gap-2">
       <Select
         label="Choose review type"
         placeholder="From Type"
@@ -224,11 +233,212 @@ function ReviewForm2() {
           setFormType(item);
         }}
       />
+      {reviewFormComponent}
     </div>
   );
 }
 
-const ReviewForm = observer(() => {
+const VentureReviewForm = observer(() => {
+  const { reviewToolModel } = useStores();
+
+  const initialValues = {
+    relevance_ai: null,
+    skills: null,
+    profile_category: null,
+    motivation: null,
+    vision: null,
+    personality: null,
+    like_to_see: null,
+    doubts: null,
+    furthercomments: null,
+  };
+
+  const schema = Yup.object().shape({
+    relevance_ai: Yup.number().required(),
+    skills: Yup.number().required(),
+    profile_category: Yup.string().required(),
+    motivation: Yup.number().required(),
+    vision: Yup.number().required(),
+    personality: Yup.number().required(),
+    like_to_see: Yup.string().required(),
+    doubts: Yup.string(),
+    furthercomments: Yup.string(),
+  });
+
+  const profileCategories = {
+    "The Technologist": "TECHNOLOGIST",
+    "The Business Mind": "BUSINESSMIND",
+    "The Domain Expert": "DOMAINEXPERT",
+    "The Creative Thinker": "CREATIVETHINKER",
+  };
+
+  const likeToSee = {
+    Yes: "YES",
+    Maybe: "MAYBE",
+    No: "NO",
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={schema}
+      onSubmit={(values: FormikValues) => {
+        reviewToolModel.submitReview(values);
+      }}
+    >
+      {({ values, setFieldValue }) => (
+        <Form className="top-0 z-0 grid h-fit grid-cols-2 gap-4 rounded-lg bg-gray-200 p-8 dark:bg-gray-600 md:sticky">
+          <h2 className="text-2xl lg:col-span-2">Submit Review</h2>
+          <div>
+            <Field
+              as={Input}
+              label="Relevance to AI or tech-related fields"
+              type="number"
+              name="relevance_ai"
+            />
+            <ErrorMessage
+              component="p"
+              className="text-red-500"
+              name="relevance_ai"
+            />
+          </div>
+          <div>
+            <Field
+              as={Input}
+              label="Skills & notable experiences"
+              type="number"
+              name="skills"
+            />
+            <ErrorMessage
+              component="p"
+              className="text-red-500"
+              name="skills"
+            />
+          </div>
+          <div className="flex flex-col">
+            <Field
+              label={"Profile category"}
+              name={`profile_category`}
+              as={Select}
+              placeholder={"Profile category"}
+              data={Object.entries(profileCategories).map(([key, value]) => ({
+                key: key,
+                value: value,
+              }))}
+              selectedItem={{
+                key: profileCategories[values["profile_category"]],
+                value: values["profile_category"],
+              }}
+              setSelectedItem={(value) => {
+                setFieldValue(`profile_category`, value);
+              }}
+            />
+            <ErrorMessage
+              component="p"
+              className="text-red-500"
+              name={`profile_category`}
+            />
+          </div>
+          <div>
+            <Field
+              as={Input}
+              label="Motivation"
+              type="number"
+              name="motivation"
+            />
+            <ErrorMessage
+              component="p"
+              className="text-red-500"
+              name="motivation"
+            />
+          </div>
+          <div>
+            <Field
+              as={Input}
+              label="Vision & Entrepreneurial Spirit"
+              type="number"
+              name="vision"
+            />
+            <ErrorMessage
+              component="p"
+              className="text-red-500"
+              name="vision"
+            />
+          </div>
+          <div>
+            <Field
+              as={Input}
+              label="Personality"
+              type="number"
+              name="personality"
+            />
+            <ErrorMessage
+              component="p"
+              className="text-red-500"
+              name="personality"
+            />
+          </div>
+          <div className="flex flex-col">
+            <Field
+              label={"Would you like to see this person at AI E-Lab?"}
+              name={`like_to_see`}
+              as={Select}
+              placeholder={"select"}
+              data={Object.entries(likeToSee).map(([key, value]) => ({
+                key: key,
+                value: value,
+              }))}
+              selectedItem={{
+                key: likeToSee[values["like_to_see"]],
+                value: values["like_to_see"],
+              }}
+              setSelectedItem={(value) => {
+                setFieldValue(`like_to_see`, value);
+              }}
+            />
+            <ErrorMessage
+              component="p"
+              className="text-red-500"
+              name={`like_to_see`}
+            />
+          </div>
+          <div className="col-start-1 md:col-span-2">
+            <Field
+              as={Textarea}
+              label="Do you have any doubts about this person’s commitment to AI E-Lab?"
+              type="number"
+              name="doubts"
+            />
+            <ErrorMessage
+              component="p"
+              className="text-red-500"
+              name="doubts"
+            />
+          </div>
+          <div className="col-start-1 md:col-span-2">
+            <Field
+              as={Textarea}
+              label="Comments for interviewing"
+              type="number"
+              name="furthercomments"
+            />
+            <ErrorMessage
+              component="p"
+              className="text-red-500"
+              name="furthercomments"
+            />
+          </div>
+
+          <Button className="lg:col-span-2" type="submit">
+            Submit review
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  );
+});
+
+const MembershipReviewForm = observer(() => {
   const { reviewToolModel } = useStores();
 
   const initialValues = {
@@ -259,7 +469,7 @@ const ReviewForm = observer(() => {
         reviewToolModel.submitReview(values);
       }}
     >
-      <Form className="grid h-fit grid-cols-2 gap-4 rounded-lg bg-gray-200 p-8 dark:bg-gray-600">
+      <Form className="top-0 grid h-fit grid-cols-2 gap-4 rounded-lg bg-gray-200 p-8 dark:bg-gray-600 md:sticky">
         <h2 className="text-2xl lg:col-span-2">Submit Review</h2>
         <div>
           <Field
