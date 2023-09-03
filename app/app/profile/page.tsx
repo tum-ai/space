7d333bb1be149999e12b505455723b267d5c35cc
Simon Huang <simon.huang@tum.de>
2023-09-03 14:53:54 +0200

    refactor: Change model in profile to fetch request (#133)
    Co-authored-by: MunzerDw <munzerdwedari@gmail.com>

--- app/app/profile/page.tsx
+++ app/app/profile/page.tsx
@@ -1,37 +1,28 @@
 "use client";
 
-import { Section } from "@components/Section";
 import ProfileOverview from "./components/ProfileOverview";
-import { useStores } from "@providers/StoreProvider";
-import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
 import { observer } from "mobx-react";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "next/navigation";
 
 const Profile = () => {
-  const { profileModel } = useStores();
-  const searchParams = useSearchParams();
-  const id = searchParams.get("id");
+  const id = useSearchParams().get("id");
 
-  useEffect(() => {
-    if (!id) return;
-    profileModel.getProfile(typeof id === "string" ? id : id[0]);
-  }, [profileModel, id]);
+  const profileQuery = useQuery({
+    queryKey: [`profile-${id}`],
+    queryFn: () => axios.get(`/profile/${id}`).then((res) => res.data),
+  });
 
-  const profile = profileModel.profile;
-
-  if (profileModel.loading) {
-    return <div>Loading...</div>;
+  if (profileQuery.isLoading) {
+    return <h1>Loading...</h1>;
   }
 
-  if (!profile) {
-    return <div>Profile not found.</div>;
+  if (profileQuery.error) {
+    return <h1>Profile not found.</h1>;
   }
 
-  return (
-    <Section>
-      <ProfileOverview profile={profile} />
-    </Section>
-  );
+  return <ProfileOverview profile={profileQuery.data.data} />;
 };
 
 export default observer(Profile);
