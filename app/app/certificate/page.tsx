04adc539345f585a5b054dae77c70a1a962f5b5d
MunzerDw <munzerdwedari@gmail.com>
2023-09-08 14:33:45 +0200

    add errors to form

--- app/app/certificate/page.tsx
+++ app/app/certificate/page.tsx
@@ -6,10 +6,10 @@ import { Section } from "@components/Section";
 import Select from "@components/Select";
 import axios, { AxiosError } from "axios";
 import download from "downloadjs";
-import { Field, Form, Formik, FormikValues } from "formik";
+import { ErrorMessage, Field, Form, Formik, FormikValues } from "formik";
 import { observer } from "mobx-react";
-import * as Yup from "yup";
 import toast from "react-hot-toast";
+import * as Yup from "yup";
 
 function Certificate() {
   const departments = [
@@ -90,71 +90,153 @@ function Certificate() {
           {({ setFieldValue }) => (
             <Form className="grid-cols2 grid gap-4">
               <h2 className="text-2xl lg:col-span-2">Create Certificate</h2>
-              <Field
-                as={Select}
-                label="Department"
-                name="DEPARTMENT"
-                placeholder={"Department"}
-                data={departments.map((department) => ({
-                  key: department,
-                  value: department,
-                }))}
-                setSelectedItem={(item: string) => {
-                  setFieldValue("DEPARTMENT", item);
-                }}
-              />
-              <Field
-                as={Select}
-                label="Position"
-                name="TITLE"
-                placeholder={"Position"}
-                data={positions.map((position) => ({
-                  key: position,
-                  value: position,
-                }))}
-                setSelectedItem={(position: string) => {
-                  setFieldValue("TITLE", position);
-                }}
-              />
-              <Field as={Input} label="First Name" type="text" name="NAME" />
-              <Field as={Input} label="Last Name" type="text" name="LASTNAME" />
-              <Field as={Input} label="Date Now" type="text" name="DATENOW" />
-              <Field
-                as={Input}
-                label="Date Joined"
-                type="text"
-                name="DATEJOINED"
-              />
-              <Field
-                as={Input}
-                label="Pronoun (his/her)"
-                type="text"
-                name="PRONOUNPOS"
-              />
-              <Field
-                as={Input}
-                label="Date Signed On"
-                type="text"
-                name="SIGNED_ON"
-              />
-              <Field
-                as={Input}
-                label="Contribution 1"
-                type="text"
-                name="CONTRIB_1"
-              />
-              <Field
-                as={Input}
-                label="Contribution 2"
-                type="text"
-                name="CONTRIB_2"
-              />
-              <Field
-                as={Input}
-                label="Contribution 3"
-                type="text"
-                name="CONTRIB_3"
-              />
+              <div>
+                <Field
+                  as={Select}
+                  label="Department"
+                  name="DEPARTMENT"
+                  placeholder={"Department"}
+                  data={departments.map((department) => ({
+                    key: department,
+                    value: department,
+                  }))}
+                  setSelectedItem={(item: string) => {
+                    setFieldValue("DEPARTMENT", item);
+                  }}
+                />
+                <ErrorMessage
+                  component="p"
+                  className="text-red-500"
+                  name="DEPARTMENT"
+                />
+              </div>
+              <div>
+                <Field
+                  as={Select}
+                  label="Position"
+                  name="TITLE"
+                  placeholder={"Position"}
+                  data={positions.map((position) => ({
+                    key: position,
+                    value: position,
+                  }))}
+                  setSelectedItem={(position: string) => {
+                    setFieldValue("TITLE", position);
+                  }}
+                />
+                <ErrorMessage
+                  component="p"
+                  className="text-red-500"
+                  name="DEPARTMENT"
+                />
+              </div>
+              <div>
+                <Field as={Input} label="First Name" type="text" name="NAME" />
+                <ErrorMessage
+                  component="p"
+                  className="text-red-500"
+                  name="NAME"
+                />
+              </div>
+              <div>
+                <Field
+                  as={Input}
+                  label="Last Name"
+                  type="text"
+                  name="LASTNAME"
+                />
+                <ErrorMessage
+                  component="p"
+                  className="text-red-500"
+                  name="LASTNAME"
+                />
+              </div>
+              <div>
+                <Field as={Input} label="Date Now" type="text" name="DATENOW" />
+                <ErrorMessage
+                  component="p"
+                  className="text-red-500"
+                  name="DATENOW"
+                />
+              </div>
+              <div>
+                <Field
+                  as={Input}
+                  label="Date Joined"
+                  type="text"
+                  name="DATEJOINED"
+                />
+                <ErrorMessage
+                  component="p"
+                  className="text-red-500"
+                  name="DATEJOINED"
+                />
+              </div>
+              <div>
+                <Field
+                  as={Input}
+                  label="Pronoun (his/her)"
+                  type="text"
+                  name="PRONOUNPOS"
+                />
+                <ErrorMessage
+                  component="p"
+                  className="text-red-500"
+                  name="PRONOUNPOS"
+                />
+              </div>
+              <div>
+                <Field
+                  as={Input}
+                  label="Date Signed On"
+                  type="text"
+                  name="SIGNED_ON"
+                />
+                <ErrorMessage
+                  component="p"
+                  className="text-red-500"
+                  name="SIGNED_ON"
+                />
+              </div>
+              <div>
+                <Field
+                  as={Input}
+                  label="Contribution 1"
+                  type="text"
+                  name="CONTRIB_1"
+                />
+                <ErrorMessage
+                  component="p"
+                  className="text-red-500"
+                  name="CONTRIB_1"
+                />
+              </div>
+              <div>
+                <Field
+                  as={Input}
+                  label="Contribution 2"
+                  type="text"
+                  name="CONTRIB_2"
+                />
+                <ErrorMessage
+                  component="p"
+                  className="text-red-500"
+                  name="CONTRIB_2"
+                />
+              </div>
+              <div>
+                <Field
+                  as={Input}
+                  label="Contribution 3"
+                  type="text"
+                  name="CONTRIB_3"
+                />
+                <ErrorMessage
+                  component="p"
+                  className="text-red-500"
+                  name="CONTRIB_3"
+                />
+              </div>
               <Button className="lg:col-span-2" type="submit">
                 save
               </Button>
