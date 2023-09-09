631a90d8ee84be73d3b6f07116d8097c403a2541
Simon Huang <simon.huang@tum.de>
2023-09-09 16:54:27 +0200

    remove loading state from components

--- app/components/Checkbox.tsx
+++ app/components/Checkbox.tsx
@@ -1,26 +1,26 @@
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 import { CheckIcon } from "@radix-ui/react-icons";
-import { clsx } from "clsx";
-import { LoadingWheel } from "./LoadingWheel";
+import { cx } from "class-variance-authority";
 
-export const Checkbox = ({ checked, onCheckedChange, loading = false }) => {
+interface Props {
+  checked: boolean;
+  onCheckedChange: (checked: boolean) => void;
+}
+
+export const Checkbox = ({ checked, onCheckedChange }: Props) => {
   return (
     <CheckboxPrimitive.Root
       checked={checked}
-      onCheckedChange={loading ? null : onCheckedChange}
-      className={clsx(
+      onCheckedChange={onCheckedChange}
+      className={cx(
         "flex h-6 w-6 items-center rounded",
         "radix-state-checked:bg-purple-600 radix-state-unchecked:bg-gray-100 dark:radix-state-unchecked:bg-gray-900",
         "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75",
       )}
     >
-      {loading ? (
-        <LoadingWheel size={"tiny"} />
-      ) : (
-        <CheckboxPrimitive.Indicator>
-          <CheckIcon className="h-6 w-6 self-center text-white" />
-        </CheckboxPrimitive.Indicator>
-      )}
+      <CheckboxPrimitive.Indicator>
+        <CheckIcon className="h-6 w-6 self-center text-white" />
+      </CheckboxPrimitive.Indicator>
     </CheckboxPrimitive.Root>
   );
 };
