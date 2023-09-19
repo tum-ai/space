bb228a2a8a66ed6c35c92929fc0e9257f9207962
Simon Huang <simon.huang@tum.de>
2023-09-19 14:00:57 +0200

    fix formatting

--- api/space_api/profiles/routes.py
+++ api/space_api/profiles/routes.py
@@ -473,7 +473,8 @@ def list_roles(request: Request) -> dict:
 
 @router.get(
     "/department-memberships",
-    response_description="List department memberships that meet the filter criteria",
+    response_description=
+    "List department memberships that meet the filter criteria",
     response_model=ResponseDepartmentMembershipWithProfileList | ErrorResponse,
 )
 @enable_paging(max_page_size=200)
