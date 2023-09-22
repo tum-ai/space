658bec65a360bd987b9459f432069dc739d66f0a
Simon Huang <simon.huang@tum.de>
2023-09-22 13:06:39 +0200

    add input states

--- app/app/review/review/_components/MembershipReviewForm.tsx
+++ app/app/review/review/_components/MembershipReviewForm.tsx
@@ -11,7 +11,7 @@ import { useRouter } from "next/navigation";
 
 export const MembershipReviewForm = ({ application, form }: FormProps) => {
   const applicationId = application?.id;
-  const initialValues = form || {
+  const initialValues: any = form || {
     motivation: null,
     skill: null,
     fit: null,
@@ -104,7 +104,7 @@ export const MembershipReviewForm = ({ application, form }: FormProps) => {
         }
       }}
     >
-      {({ values, setFieldValue }) => (
+      {({ touched, errors, values, setFieldValue }) => (
         <Form className="grid h-fit gap-4 rounded-lg bg-gray-200 p-8 dark:bg-gray-600 md:grid-cols-2">
           <h2 className="text-2xl lg:col-span-2">Submit Review</h2>
           <div>
@@ -113,6 +113,7 @@ export const MembershipReviewForm = ({ application, form }: FormProps) => {
               label="Motivation"
               type="number"
               name="motivation"
+              state={touched.motivation && errors.motivation && "error"}
               fullWidth
             />
             <ErrorMessage
@@ -128,6 +129,7 @@ export const MembershipReviewForm = ({ application, form }: FormProps) => {
               label="Skill"
               type="number"
               name="skill"
+              state={touched.skill && errors.skill && "error"}
               fullWidth
             />
             <ErrorMessage component="p" className="text-red-500" name="skill" />
@@ -139,6 +141,7 @@ export const MembershipReviewForm = ({ application, form }: FormProps) => {
               label="Overall fit"
               type="number"
               name="fit"
+              state={touched.fit && errors.fit && "error"}
               fullWidth
             />
             <ErrorMessage component="p" className="text-red-500" name="fit" />
@@ -148,6 +151,7 @@ export const MembershipReviewForm = ({ application, form }: FormProps) => {
             <Field
               label={"Fit in TUM.ai?"}
               name={`in_tumai`}
+              state={touched.in_tumai && errors.in_tumai && "error"}
               as={Select}
               placeholder={"select"}
               options={Object.entries(likeToSee).map(([key, value]) => ({
@@ -175,6 +179,9 @@ export const MembershipReviewForm = ({ application, form }: FormProps) => {
               label="Tum.ai fit comment"
               type="text"
               name="comment_fit_tumai"
+              state={
+                touched.comment_fit_tumai && errors.comment_fit_tumai && "error"
+              }
               fullWidth
             />
             <ErrorMessage
@@ -190,6 +197,7 @@ export const MembershipReviewForm = ({ application, form }: FormProps) => {
               label="Time commitment"
               type="text"
               name="timecommit"
+              state={touched.timecommit && errors.timecommit && "error"}
               fullWidth
             />
             <ErrorMessage
@@ -205,6 +213,7 @@ export const MembershipReviewForm = ({ application, form }: FormProps) => {
               label="Department 1 score"
               type="number"
               name="dept1_score"
+              state={touched.dept1_score && errors.dept1_score && "error"}
               fullWidth
             />
             <ErrorMessage
@@ -220,6 +229,7 @@ export const MembershipReviewForm = ({ application, form }: FormProps) => {
               label="Department 2 score"
               type="number"
               name="dept2_score"
+              state={touched.dept2_score && errors.dept2_score && "error"}
               fullWidth
             />
             <ErrorMessage
@@ -235,6 +245,7 @@ export const MembershipReviewForm = ({ application, form }: FormProps) => {
               label="Department 3 score"
               type="number"
               name="dept3_score"
+              state={touched.dept3_score && errors.dept3_score && "error"}
               fullWidth
             />
             <ErrorMessage
@@ -250,6 +261,7 @@ export const MembershipReviewForm = ({ application, form }: FormProps) => {
               label="Good fit?"
               type="text"
               name="maybegoodfit"
+              state={touched.maybegoodfit && errors.maybegoodfit && "error"}
               fullWidth
             />
             <ErrorMessage
@@ -265,6 +277,9 @@ export const MembershipReviewForm = ({ application, form }: FormProps) => {
               label="Further comments"
               type="text"
               name="furthercomments"
+              state={
+                touched.furthercomments && errors.furthercomments && "error"
+              }
               fullWidth
             />
             <ErrorMessage
