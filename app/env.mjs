d514e8590109ad7f5f92ea596bdae20a1f7d6c3d
Robin Holzinger <nerotycs@gmail.com>
2023-10-14 18:03:46 +0200

    Add Auth0 env variables'

--- app/env.mjs
+++ app/env.mjs
@@ -11,6 +11,13 @@ export const env = createEnv({
   client: {
     NEXT_PUBLIC_API_URL: z.string().url(),
     NEXT_PUBLIC_ENVIRONMENT: z.enum(["development", "production", "preview"]),
+    NEXT_PUBLIC_AUTH0_SECRET: z.string(),
+    NEXT_PUBLIC_AUTH0_BASE_URL: z.string().url(),
+    NEXT_PUBLIC_AUTH0_ISSUER_BASE_URL: z.string().url(),
+    NEXT_PUBLIC_AUTH0_CLIENT_ID: z.string(),
+    NEXT_PUBLIC_AUTH0_CLIENT_SECRET: z.string(),
+    NEXT_PUBLIC_AUTH0_AUDIENCE: z.string(),
+    NEXT_PUBLIC_AUTH0_SCOPE: z.string(),
   },
   /*
    * Due to how Next.js bundles environment variables on Edge and Client,
@@ -21,5 +28,12 @@ export const env = createEnv({
   runtimeEnv: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_ENVIRONMENT: process.env.NEXT_PUBLIC_ENVIRONMENT,
+    NEXT_PUBLIC_AUTH0_SECRET: process.env.NEXT_PUBLIC_AUTH0_SECRET,
+    NEXT_PUBLIC_AUTH0_BASE_URL: process.env.NEXT_PUBLIC_AUTH0_BASE_URL,
+    NEXT_PUBLIC_AUTH0_ISSUER_BASE_URL: process.env.NEXT_PUBLIC_AUTH0_ISSUER_BASE_URL,
+    NEXT_PUBLIC_AUTH0_CLIENT_ID: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID,
+    NEXT_PUBLIC_AUTH0_CLIENT_SECRET: process.env.NEXT_PUBLIC_AUTH0_CLIENT_SECRET,
+    NEXT_PUBLIC_AUTH0_SCOPE: process.env.NEXT_PUBLIC_AUTH0_SCOPE,
+    NEXT_PUBLIC_AUTH0_AUDIENCE: process.env.NEXT_PUBLIC_AUTH0_AUDIENCE,
   },
 });
