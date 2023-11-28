f3aa7cfdc1b05a34c9677a20f22820c841121d4b
behsadriemer <behsad.riemer@icloud.com>
2023-11-28 03:48:43 +0100

    Keep first_name consistent in prisma schema

--- app/app/api/auth/[...nextauth]/route.ts
+++ app/app/api/auth/[...nextauth]/route.ts
@@ -7,12 +7,26 @@ import prisma from "database/db";
 export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma),
     secret: process.env.NEXTAUTH_SECRET,
+    pages: {
+        signIn: '/auth',
+    },
     providers: [
         SlackProvider(
             {
                 clientId: process.env.SLACK_CLIENT_ID,
                 clientSecret: process.env.SLACK_CLIENT_SECRET,
-            }
+                profile(profile, tokens) {
+                    return {
+                        id: profile["https://slack.com/user_id"] || profile.sub,
+                        email: profile.email,
+                        image: profile["https://slack.com/user_image_192"],
+                        first_name: profile.given_name,
+                        permission: 'member',
+                        last_name: profile.family_name,
+                        emailVerified: profile.email_verified
+                    };
+                },
+            },
         ),
     ],
 }
