1041e8b9cff0e278763d9b2235c2c1912b007a7a
Cyro292 <amon@archi.de>
2023-12-03 00:15:46 +0100

    update user -api

--- app/app/api/profiles/route.ts
+++ app/app/api/profiles/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server'
 import prisma from "database/db";
+import { error } from 'console';
 
 export async function GET(request: Request, context: { params }) {
     const profiles = await prisma.user.findMany({
@@ -26,6 +27,54 @@ export async function GET(request: Request, context: { params }) {
     return NextResponse.json({"profiles": profiles}, { status: 200 })
 }
 
+
+export async function PUT(req: Request, params: { id: string }) {
+    
+
+    let payload;
+    console.log(req.body);
+    try {
+        payload = await req.json();
+    } catch (err) {
+        return NextResponse.json({ error: "Invalid JSON" }, { status: 400 });
+    }
+
+
+    const { id } = await params;
+    if (!id) {
+        return NextResponse.json({ error: "Missing id " }, { status: 400 });
+    }
+    const { first_name, last_name, email, permission, department_memberships } = payload;
+
+    const data: any = {};
+
+    if (first_name) data.first_name = first_name;
+    if (last_name) data.last_name = last_name;
+    if (email) data.email = email;
+    if (permission) data.permission = permission;
+
+    const profiles = await prisma.user.update({
+        where: {
+            id: String(id),
+        },
+        data: {
+            first_name,
+            last_name,
+            email,
+            permission,
+        },
+        select: {
+            id: true,
+            first_name: true,
+            last_name: true,
+            email: true,
+            image: true,
+            permission: true,
+        }
+    });
+
+    return NextResponse.json({"profiles": profiles}, { status: 200 })
+}
 
 
 
