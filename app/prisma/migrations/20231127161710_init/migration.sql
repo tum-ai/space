3c3d510fb880376028696ccb5673d215eb05f126
behsadriemer <behsad.riemer@icloud.com>
2023-12-03 19:59:18 +0100

    Add password to user model in prisma schema and squash migrations into one

--- app/prisma/migrations/20231127161710_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `state` to the `Session` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Session" ADD COLUMN     "state" TEXT NOT NULL;
