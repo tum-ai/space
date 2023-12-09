25f4aa1635eb308171e85407b2a77082c884e61e
brebribre <70563428+brebribre@users.noreply.github.com>
2023-12-09 14:14:22 +0100

    [DEV-313] use sections instead of container

--- app/app/dashboard/[opportunity_id]/page.tsx
+++ app/app/dashboard/[opportunity_id]/page.tsx
@@ -7,64 +7,65 @@ import ClickableInfoCard from "../components/ClickableInfoCard";
 import {PersonIcon} from "@radix-ui/react-icons";
 import { Card } from "@components/ui/card";
 import {Button} from "@components/ui/button"
+import {Section} from "@components/Section";
 export default function Dashboard({ params }) {
     const opportunityId = decodeURIComponent(params.opportunity_id);
-
     const [selectedPhase, setSelectedPhase] = useState("SCREENING")
     return (
-        <div className="flex flex-col gap-8 container">
-            <div className="flex flex-col gap-4">
-                <h1 className="font-thin text-6xl">Dashboard</h1>
-                <p>ID: {opportunityId}</p>
-                <p className="text-2xl mt-5">General Overview</p>
-
-                <div className="grid grid-cols-3 gap-4">
-                    <DetailedInfoCard title="Total Applicants" data={420} growthInPercent={10}>
-                        <PersonIcon className="w-10 h-10"/>
-                    </DetailedInfoCard>
-                    <DetailedInfoCard title="Total Applicants" data={420} growthInPercent={10}>
-                        <PersonIcon className="w-10 h-10"/>
-                    </DetailedInfoCard>
-                    <DetailedInfoCard title="Total Applicants" data={420} growthInPercent={10}>
-                        <PersonIcon className="w-10 h-10"/>
-                    </DetailedInfoCard>
-                </div>
-
-                <AreaGraph id={opportunityId}/>
-
+        <Section>
+            <div className="flex flex-col gap-8">
                 <div className="flex flex-col gap-4">
-                    <p className="text-2xl mt-5">Overview of phases</p>
-                    <div className="grid grid-cols-6 gap-4">
-                        <Select
-                            placeholder="From Type"
-                            options={[
-                                {
-                                    key: "Screening",
-                                    value: "SCREENING",
-                                },
-                                {
-                                    key: "Interview",
-                                    value: "INTERVIEW",
-                                },
-                            ]}
-                            value={selectedPhase}
-                            setSelectedItem={(item:string)=>{setSelectedPhase(item)}}
-                        />
-                        <Button className="col-start-6">Visit</Button>
+                    <h1 className="font-thin text-6xl">Dashboard</h1>
+                    <p>ID: {opportunityId}</p>
+                    <p className="text-2xl mt-5">General Overview</p>
+
+                    <div className="grid grid-cols-3 gap-4">
+                        <DetailedInfoCard title="Total Applicants" data={420} growthInPercent={10}>
+                            <PersonIcon className="w-10 h-10"/>
+                        </DetailedInfoCard>
+                        <DetailedInfoCard title="Total Applicants" data={420} growthInPercent={10}>
+                            <PersonIcon className="w-10 h-10"/>
+                        </DetailedInfoCard>
+                        <DetailedInfoCard title="Total Applicants" data={420} growthInPercent={10}>
+                            <PersonIcon className="w-10 h-10"/>
+                        </DetailedInfoCard>
                     </div>
-                    <div className="grid grid-cols-6 gap-4">
-                        <div className="col-span-1 flex flex-col gap-4">
-                            <ClickableInfoCard data={30} description={"Screeners"} dark={true}/>
-                            <ClickableInfoCard data={30} description={"Screeners"}/>
+
+                    <AreaGraph id={opportunityId}/>
+
+                    <div className="flex flex-col gap-4">
+                        <p className="text-2xl mt-5">Overview of phases</p>
+                        <div className="grid grid-cols-6 gap-4">
+                            <Select
+                                placeholder="From Type"
+                                options={[
+                                    {
+                                        key: "Screening",
+                                        value: "SCREENING",
+                                    },
+                                    {
+                                        key: "Interview",
+                                        value: "INTERVIEW",
+                                    },
+                                ]}
+                                value={selectedPhase}
+                                setSelectedItem={(item:string)=>{setSelectedPhase(item)}}
+                            />
+                            <Button className="col-start-6">Visit</Button>
                         </div>
-                        <div className="col-span-5">
-                            <Card className="h-full"/>
+                        <div className="grid grid-cols-6 gap-4">
+                            <div className="col-span-1 flex flex-col gap-4">
+                                <ClickableInfoCard data={30} description={"Screeners"} dark={true}/>
+                                <ClickableInfoCard data={30} description={"Screeners"}/>
+                            </div>
+                            <div className="col-span-5">
+                                <Card className="h-full"/>
+                            </div>
                         </div>
                     </div>
                 </div>
-
-
             </div>
-        </div>
-    );
+        </Section>
+
+);
 }
