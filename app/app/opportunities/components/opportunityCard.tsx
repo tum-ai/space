1704490759ff1ce109832954cca4f45c96bc94e2
Bryan Alvin <70563428+brebribre@users.noreply.github.com>
2023-12-12 21:17:56 +0100

    feat: create opportunity page and dashboard page (#183)

---
+++ app/app/opportunities/components/opportunityCard.tsx
@@ -0,0 +1,32 @@
+import { Card, CardFooter, CardHeader, CardContent } from "@components/ui/card";
+import {Button} from "@components/ui/button";
+import {CalendarIcon} from "@radix-ui/react-icons"
+import Link from "next/link";
+export default function OpportunityCard(props:any) {
+    return (
+            <Card className="flex flex-col">
+                <CardHeader className="text-3xl">{props.opportunity.title}</CardHeader>
+                <CardContent className="flex flex-col gap-4">
+                    <span className="flex flex-row gap-2 items-center text-slate-500">
+                        <CalendarIcon/>
+                        <p>{props.opportunity.date}</p>
+                    </span>
+                    <p className="text-2xl">
+                        {props.opportunity.participants} Applicants
+                    </p>
+                    {props.opportunity.description}
+                </CardContent>
+                <CardFooter className="flex flex-cols gap-4">
+                    <Link href={"./dashboard/" + + props.opportunity.id} className="w-full">
+                        <Button className="w-full">Dashboard</Button>
+                    </Link>
+                    <Link href={"./edit/" + + props.opportunity.id} className="w-full">
+                        <Button className="w-full">Edit</Button>
+                    </Link>
+                    <Link href={"./view/" + + props.opportunity.id} className="w-full">
+                        <Button className="w-full">View</Button>
+                    </Link>
+                </CardFooter>
+            </Card>
+    );
+}
