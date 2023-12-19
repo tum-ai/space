c7fca6143ca9988863277ad0bf56cd6f1ec64248
Sofiia Danylchenko <sofiya.dan@gmail.com>
2023-12-19 19:28:22 +0100

    feat: set up database and auth (#178)
    Co-authored-by: Cyro292 <amon@archi.de>
    Co-authored-by: simonhng <simon.huang@tum.de>
    Co-authored-by: Behsad Riemer <54889278+BehsadRiemer@users.noreply.github.com>

---
+++ app/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,109 @@
+import { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import SlackProvider from "next-auth/providers/slack";
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import prisma from "database/db";
+import CredentialsProvider from "next-auth/providers/credentials";
+import EmailProvider from "next-auth/providers/email";
+import { compare } from "bcrypt";
+
+export const authOptions: NextAuthOptions = {
+    session:{
+        strategy:"jwt",
+        maxAge: 30 * 24 * 60 * 60 //30 days
+    },
+    adapter: PrismaAdapter(prisma),
+    secret: process.env.NEXTAUTH_SECRET,
+    pages: {
+        signIn: '/auth',
+        /*
+        newUser: '/auth/signup',
+        signOut: '/auth/signout',
+        error: '/auth/error', // Error code passed in query string as ?error=
+        verifyRequest: '/auth/verify-request', // (used for check email message)
+        */
+        //TODO: add signOut, error pages
+    },
+    providers: [
+        SlackProvider(
+            {
+                clientId: process.env.SLACK_CLIENT_ID,
+                clientSecret: process.env.SLACK_CLIENT_SECRET,
+                profile(profile, tokens) {
+                    return {
+                        id: profile["https://slack.com/user_id"] || profile.sub,
+                        email: profile.email,
+                        image: profile.picture,
+                        first_name: profile.given_name,
+                        permission: 'member',
+                        last_name: profile.family_name,
+                        emailVerified: profile.date_email_verified
+                    };
+                },
+            },
+        ),
+        //TODO: add email provider setup
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+              email: { label: "Email", type: "email", placeholder: "max.mustermann@gmail.com" },
+              password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials) {
+              if(!credentials?.email || !credentials?.password) {
+                  return null;
+              }
+        
+              const existingUser = await prisma.user.findUnique({
+                  where: { email : credentials?.email }
+              });
+              
+              if(!existingUser) {
+                  return null;
+              }
+  
+              const passwordMatch = await compare(credentials.password, existingUser.password);
+  
+              if(!passwordMatch) {
+                  return null;
+              }
+  
+              return {
+                  id: `${existingUser.id}`,
+                  username: existingUser.first_name + "_" + existingUser.last_name, //do we really need this here? 
+                  email: existingUser.email
+              }
+            }
+          }),
+          // EmailProvider({
+          //     server: process.env.EMAIL_SERVER,
+          //     from: process.env.EMAIL_FROM
+          // }),
+    ],
+    callbacks:{
+        async jwt({token,user}){
+            return{...token,...user}
+        },
+        async session({session,token}){
+            return {
+                ...session,
+                user : {
+                    ...session.user,
+                    id: token.id,
+                    first_name: token.first_name,
+                    permission: token.permission,
+                    image: token.image,
+                }
+            }
+            // session.user.id = token.id
+            // session.user.first_name = token.first_name
+            // session.user.permission = token.permission
+            // session.user.image = token.image
+            // return session
+        }
+    }
+}
+
+const handler = NextAuth(authOptions)
+
+export {handler as GET, handler as POST}
