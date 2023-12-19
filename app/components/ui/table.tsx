20fb8b3772ed07e5c77d5f224840e7f065ac2350
brebribre <70563428+brebribre@users.noreply.github.com>
2023-12-19 14:38:27 +0100

    align positioning

--- app/components/ui/table.tsx
+++ app/components/ui/table.tsx
@@ -70,7 +70,7 @@ const TableHead = React.forwardRef<
   <th
     ref={ref}
     className={cn(
-      "h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
+      "py-2 text-left font-medium text-muted-foreground [&:has([role=checkbox])]:pl-4",
       className
     )}
     {...props}
@@ -84,7 +84,7 @@ const TableCell = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <td
     ref={ref}
-    className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)}
+    className={cn("py-4 pl-4 [&:has([role=checkbox])]:pl-4", className)}
     {...props}
   />
 ))
