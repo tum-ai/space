17886a965d3c564cd36030a147116f3ad3c12b72
Behsad Riemer <54889278+BehsadRiemer@users.noreply.github.com>
2023-12-19 19:22:36 +0100

    feat: add slack authentication (#179)
    Co-authored-by: Sofiia Danylchenko <sofiya.dan@gmail.com>
    Co-authored-by: simonhng <simon.huang@tum.de>

--- app/app/auth/components/LoginForm.tsx
+++ app/app/auth/components/LoginForm.tsx
@@ -1,81 +1,27 @@
 "use client";
 import { Button } from "@components/ui/button";
-import Input from "@components/Input";
-import { Section } from "@components/Section";
-import { auth } from "@config/firebase";
-import * as Yup from "yup";
-import { signInWithEmailAndPassword } from "firebase/auth";
 import { useRouter } from "next/navigation";
-import toast from "react-hot-toast";
-import { Field, Form, Formik } from "formik";
-import ErrorMessage from "@components/ErrorMessage";
-import { signIn } from 'next-auth/react';
+import { signIn, signOut } from "next-auth/react";
 
-export const LoginForm = ({ setResetPassword }) => {
+
+
+export const LoginForm = async ({ setResetPassword }) => {
   const router = useRouter();
-  return (
-    <Section>
-      <Formik
-        validationSchema={Yup.object().shape({
-          email: Yup.string().email().required(),
-          password: Yup.string().required(),
-        })}
-        initialValues={{
-          email: "",
-          password: "",
-        }}
-        onSubmit = {async (values) => {
-          const signInData = await signIn('credentials', {
-            email: values.email,
-            password: values.password,
-            redirect: false
-          });
-          if(signInData?.error) {
-            console.log(signInData.error);
-          } else {
-            return router.push("/");
-          }
-        }}
-      >
-        {({ errors, touched }) => (
-          <Form className="m-auto flex max-w-[500px] flex-col gap-4">
-            <h2 className="text-3xl">Login</h2>
-            <div>
-              <Field
-                as={Input}
-                label="Email"
-                type="email"
-                name="email"
-                placeholder="daniel.korth@tum.com"
-                state={touched.email && errors.email && "error"}
-                fullWidth
-              />
-              <ErrorMessage name="email" />
-            </div>
-            <div>
-              <Field
-                as={Input}
-                label="Password"
-                type="password"
-                name="password"
-                state={touched.password && errors.password && "error"}
-                fullWidth
-              />
-              <ErrorMessage name="password" />
-            </div>
-            <hr className="col-span-2" />
-            <Button type="submit">Log in</Button>
 
-            <Button
-              variant="link"
-              type="button"
-              onClick={() => setResetPassword(true)}
-            >
-              Forgot password?
-            </Button>
-          </Form>
-        )}
-      </Formik>
-    </Section>
+  const profileRoute = () => {
+    router.push('/profile');
+  }
+
+  return (
+    <div className="m-auto flex max-w-[500px] flex-col gap-4 ">
+      <h2 className="text-3xl flex justify-center">Login</h2>
+      <hr className="col-span-2" />
+      <Button className="flex items-center justify-center px-4 py-2 border border-transparent rounded-lg shadow-sm text-sm font-medium text-black bg-white" onClick={() => signIn('slack')}>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Slack_icon_2019.svg/2048px-Slack_icon_2019.svg.png" alt="Slack logo" className="h-6 w-6 mr-2"/>
+        Log in with Slack
+      </Button>
+      <Button onClick={() => signOut()}> Sign out</Button>
+      <Button onClick={profileRoute}>View Profile</Button>
+    </div>
   );
 };
