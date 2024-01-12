6a81c40ead1de755be856472b7252870f5f2ee47
behsadriemer <behsad.riemer@icloud.com>
2024-01-12 22:34:40 +0100

    Sign In redo - attempt 2

--- app/app/api/auth/[...nextauth]/route.ts
+++ app/app/api/auth/[...nextauth]/route.ts
@@ -6,6 +6,7 @@ import prisma from "database/db";
 import CredentialsProvider from "next-auth/providers/credentials";
 import EmailProvider from "next-auth/providers/email";
 import { compare } from "bcrypt";
+import Error from "next/error";
 
 export const authOptions: NextAuthOptions = {
   session: {
@@ -21,15 +22,10 @@ export const authOptions: NextAuthOptions = {
     SlackProvider({
       clientId: process.env.SLACK_CLIENT_ID,
       clientSecret: process.env.SLACK_CLIENT_SECRET,
-      profile(profile, tokens) {
+      async profile(profile) {
         return {
           id: profile["https://slack.com/user_id"] || profile.sub,
-          email: profile.email,
-          image: profile.picture,
-          first_name: profile.given_name,
-          permission: "member",
-          last_name: profile.family_name,
-          emailVerified: profile.date_email_verified,
+          email: profile.email
         };
       },
     }),
@@ -68,25 +64,93 @@ export const authOptions: NextAuthOptions = {
 
         return {
           id: `${existingUser.id}`,
-          username: existingUser.first_name + "_" + existingUser.last_name, //do we really need this here?
+          username: existingUser.firstName + "_" + existingUser.lastName,
           email: existingUser.email,
         };
       },
     }),
   ],
   callbacks: {
+    async signIn({ user, profile: pro, account: acc }: {user, profile, account}) {
+      // Start a transaction
+      await prisma.$transaction(async (prisma) => {
+        const userExists = await prisma.user.findFirst({
+          where: { 
+            accounts: {
+              some: {
+                id: user.id 
+              }
+            },
+        }})
+
+        console.log(userExists)
+
+        console.log("Here;s the user",user)
+
+        //TODO ensure that you check the role!
+
+        if (!userExists) {
+          try{
+            const userPersi = await prisma.user.create({
+              data: {
+                email: pro.email,
+                firstName: pro.given_name,
+                lastName: pro.family_name,
+                image: pro.picture,
+                emailVerified: new Date(pro.date_email_verified*1000),
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                userRoles: {connect: [{name: "member"}]}
+              }
+            })
+
+            const toke = acc.access_token
+            console.log(toke)
+
+            await prisma.account.create({
+              data: {
+                userId: userPersi.id,
+                idToken: acc.id_token,
+                type: acc.type,
+                provider: acc.provider,
+                providerAccountId: acc.providerAccountId,
+                ok: acc.ok,
+                state: acc.state,
+                accessToken: toke,
+                tokenType: acc.token_type,
+                createdAt: new Date(acc.createdAt),
+                updatedAt: new Date(acc.updatedAt)
+              }
+            })
+          }
+          catch(Error){
+
+            console.log("\n\n\n\n\n",Error)
+          }
+
+          //TODO destructure hwere possible
+          }
+          return userExists;
+        });
+
+      return true; 
+    },
     async jwt({ token, user }) {
-      return { ...token, ...user };
+      if (user) {
+        return {...token, ...user}
+      }
+      return token;
     },
     async session({ session, token }) {
+      //TODO print this out
       return {
         ...session,
         user: {
           ...session.user,
           id: token.id,
-          first_name: token.first_name,
-          permission: token.permission,
-          image: token.image,
+          firstName: token.firstName,
+          lastName: token.lastName,
+          image: token.image
         },
       };
     },
