19dc8ce36d57ed3c47689eab87037347e7545fcb
Stanislav Opeida <stanislav.opeida@gmail.com>
2024-01-17 02:52:06 +0100

    feat: service for tally applications

--- app/app/env.mjs
+++ app/app/env.mjs
@@ -11,6 +11,7 @@ export const env = createEnv({
   client: {
     NEXT_PUBLIC_API_URL: z.string().url(),
     NEXT_PUBLIC_ENVIRONMENT: z.enum(["development", "production", "preview"]),
+    NEXT_PUBLIC_TALLY_SIGNING_SECRET: z.string(),
   },
   /*
    * Due to how Next.js bundles environment variables on Edge and Client,
@@ -21,5 +22,7 @@ export const env = createEnv({
   runtimeEnv: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_ENVIRONMENT: process.env.NEXT_PUBLIC_ENVIRONMENT,
+    NEXT_PUBLIC_TALLY_SIGNING_SECRET:
+      process.env.NEXT_PUBLIC_TALLY_SIGNING_SECRET,
   },
 });
