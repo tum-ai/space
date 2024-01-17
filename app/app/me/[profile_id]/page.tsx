08a1e08d395b2e3540ccad5fa4052b9ae0fba10d
Max-vS <max@vonstorch.com>
2024-01-17 20:38:03 +0100

    restrucure data

--- app/app/me/[profile_id]/page.tsx
+++ app/app/me/[profile_id]/page.tsx
@@ -23,34 +23,57 @@ const Me = () => {
     return <h1>Profile not found.</h1>;
   }
   */
+
   const data = {
+    id: "1",
+    email: "max@mustermann.com",
+    image: "https://placekitten.com/200/200",
+    activity_status: "active",
+    current_department: "DEV",
+    current_department_position: "Team Lead",
+    first_name: "Max",
+    last_name: "Mustermann",
+    nationality: "German",
+    birthday: new Date("2023-12-18"),
+    university: "TUM",
+    degree_level: "Bachelor",
+    degree_name: "Informatics",
+    degree_semester: 100,
+    description:
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+  };
+
+  const { id, email, image, activity_status, current_department, current_department_position, 
+    first_name, last_name, nationality, birthday, university, degree_level, 
+    degree_name, degree_semester, description } = data;
+
+  const restructured = {
     general: {
-      id: "1",
-      email: "max@mustermann.com",
-      image: "https://placekitten.com/200/200",
-      activity_status: "active",
-      current_department: "DEV",
-      current_department_position: "Team Lead",
+      id,
+      email,
+      image,
+      activity_status,
+      current_department,
+      current_department_position,
     },
     grid: {
       personal: {
-        first_name: "Max",
-        last_name: "Mustermann",
-        nationality: "German",
-        birthday: new Date("2023-12-18"),
+        first_name,
+        last_name,
+        nationality,
+        birthday: new Date(birthday),
       },
       academia: {
-        university: "TUM",
-        degree_level: "Bachelor",
-        degree_name: "Informatics",
-        degree_semester: 100,
+        university,
+        degree_level,
+        degree_name,
+        degree_semester,
       },
-      description:
-        "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+      description,
     },
   };
 
-  return <ProfileOverview profile={data} />;
+  return <ProfileOverview profile={restructured} />;
 };
 
 export default Me;
