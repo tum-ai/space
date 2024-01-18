8acd2801a25da88b88c307f6919caa5c49d78269
Max-vS <max@vonstorch.com>
2024-01-18 16:30:06 +0100

    bryans tabs

--- app/app/reviews/[opportunity_id]/page.tsx
+++ app/app/reviews/[opportunity_id]/page.tsx
@@ -1,13 +1,17 @@
 import { Section } from "@components/Section";
-import Tabs from "@components/Tabs";
 import { Button } from "@components/ui/button";
-import { TableIcon, RowsIcon, MixerHorizontalIcon } from "@radix-ui/react-icons";
+import {
+  TableIcon,
+  RowsIcon,
+  MixerHorizontalIcon,
+} from "@radix-ui/react-icons";
+import { Tabs, TabsList, TabsTrigger } from "@components/ui/tabs";
 
 export default function ReviewOverview() {
   return (
     <Section className="space-y-6">
       <OverviewHeader />
-	  <OverviewToolBar />
+      <OverviewToolBar />
     </Section>
   );
 }
@@ -15,35 +19,36 @@ export default function ReviewOverview() {
 function OverviewHeader() {
   return (
     <div className="flex flex-col gap-2">
-	<div className="flex flex-row w-full justify-between items-center">
-		<h1 className="text-6xl font-thin">Applications</h1>
-		<Button>
-			All opportunities
-		</Button>
-	</div>
+      <div className="flex w-full flex-row items-center justify-between">
+        <h1 className="text-6xl font-thin">Applications</h1>
+        <Button>All opportunities</Button>
+      </div>
       <p>View all availavle Applications</p>
-	  <h2 className="text-2xl">Winter Semester 2024/2025 Application</h2>
+      <h2 className="text-2xl">Winter Semester 2024/2025 Application</h2>
     </div>
   );
 }
 
 function OverviewToolBar() {
-	return (
-		<div className="flex flex-row w-full space-x-2 h-20">
-			<Button variant="outline" size="default" className="w-full">
-				Filter
-			</Button>
-			<Button variant="outline" size="default">
-				<MixerHorizontalIcon className="mr-2"/>
-				Filter
-			</Button>
-			<Button variant="outline" size="default" className="rounded-full">
-				<RowsIcon />
-			</Button>
-			<Button variant="outline" size="default" className="rounded-full">
-				<TableIcon />
-			</Button>
-			<Tabs />
-		</div>
-	);
+  return (
+    <div className="flex h-20 w-full flex-row space-x-2">
+      <Button variant="outline" size="default" className="w-full">
+        Filter
+      </Button>
+      <Button variant="outline" size="default">
+        <MixerHorizontalIcon className="mr-2" />
+        Filter
+      </Button>
+      <Tabs defaultValue="rows">
+        <TabsList className="h-10">
+          <TabsTrigger value="rows">
+            <RowsIcon />
+          </TabsTrigger>
+          <TabsTrigger value="table">
+            <TableIcon />
+          </TabsTrigger>
+        </TabsList>
+      </Tabs>
+    </div>
+  );
 }
