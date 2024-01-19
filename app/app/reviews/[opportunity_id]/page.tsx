6bce1c538ef5faa2fb0ac16eea80633f4dc5435b
Max-vS <max@vonstorch.com>
2024-01-19 14:25:10 +0100

    Row-View for reviews

--- app/app/reviews/[opportunity_id]/page.tsx
+++ app/app/reviews/[opportunity_id]/page.tsx
@@ -1,40 +1,122 @@
+"use client";
+
 import { Section } from "@components/Section";
 import { Button } from "@components/ui/button";
+import OverviewRows from "./components/OverviewRows";
+import OverviewTable from "./components/OverviewRows";
 import {
   TableIcon,
   RowsIcon,
   MixerHorizontalIcon,
-  MagnifyingGlassIcon,
 } from "@radix-ui/react-icons";
 import { Tabs, TabsList, TabsTrigger } from "@components/ui/tabs";
 import Input from "@components/Input";
 import Link from "next/link";
+import { useState } from "react";
+import OpportunityCard from "app/opportunities/components/opportunityCard";
 
-export default function ReviewOverview() {
+interface Reviewer {
+  name: string;
+  imgSrc: string;
+}
+
+let simon: Reviewer = {
+  name: "Simon Huang",
+  imgSrc: "https://placekitten.com/204/204",
+};
+let bryan: Reviewer = {
+  name: "Bryan Alvin",
+  imgSrc: "https://placekitten.com/201/201",
+};
+let tim: Reviewer = {
+  name: "Tim Baum",
+  imgSrc: "https://placekitten.com/203/203",
+};
+
+const data = {
+  "1": {
+    firstName: "Simon",
+    lastName: "Huang",
+    phase: "Interview",
+    score: 9.8,
+    reviewer: [simon, bryan],
+  },
+  "2": {
+    firstName: "Tim",
+    lastName: "Baum",
+    phase: "Interview",
+    score: 6.8,
+    reviewer: [simon, tim],
+  },
+  "3": {
+    firstName: "Bryan",
+    lastName: "Alvin",
+    phase: "Interview",
+    score: 7.3,
+    reviewer: [simon, tim, bryan],
+  },
+  "4": {
+    firstName: "Peter",
+    lastName: "Petersen",
+    phase: "Interview",
+    score: 7.3,
+    reviewer: [simon, tim, bryan],
+  },
+  "5": {
+    firstName: "Dieter",
+    lastName: "Dietgen",
+    phase: "Interview",
+    score: 7.3,
+    reviewer: [simon, tim, bryan],
+  },
+  "6": {
+    firstName: "Magnus",
+    lastName: "Magnusen",
+    phase: "Interview",
+    score: 7.3,
+    reviewer: [simon, tim, bryan],
+  },
+};
+
+export default function ReviewOverview({ params }) {
+  const [tab, setTab] = useState<"rows" | "table">("rows");
+  const opportunityId = decodeURIComponent(params.opportunity_id);
   return (
     <Section className="space-y-6">
-      <OverviewHeader />
-      <OverviewToolBar />
+      <OverviewHeader opportunityId={opportunityId} />
+      <OverviewToolBar onTabChange={setTab} />
+      {tab === "rows" ? (
+        <OverviewRows data={data} />
+      ) : (
+        <OverviewTable data={data} />
+      )}
     </Section>
   );
 }
 
-function OverviewHeader() {
+function OverviewHeader({ opportunityId }) {
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-1.5">
       <div className="flex w-full flex-row items-center justify-between">
         <h1 className="text-6xl font-thin">Applications</h1>
         <Link href="../opportunities">
           <Button>All opportunities</Button>
         </Link>
       </div>
-      <p>View all availavle Applications</p>
-      <h2 className="text-2xl">Winter Semester 2024/2025 Application</h2>
+      <p>View all available Applications for</p>
+      <div className="flex w-full items-center justify-between">
+        <h2 className="text-2xl">
+          Winter Semester 2024/2025 Application
+        </h2>
+        <h2 className="text-2xl">
+          ID: {opportunityId}
+        </h2>
+      </div>
     </div>
   );
 }
 
-function OverviewToolBar() {
+function OverviewToolBar({ onTabChange }) {
   return (
     <div className="flex h-20 w-full flex-row space-x-2">
       <Input fullWidth placeholder="Search"></Input>
@@ -44,22 +126,14 @@ function OverviewToolBar() {
       </Button>
       <Tabs defaultValue="rows">
         <TabsList className="h-10">
-          <TabsTrigger value="rows">
+          <TabsTrigger value="rows" onClick={() => onTabChange("rows")}>
             <RowsIcon />
           </TabsTrigger>
-          <TabsTrigger value="table">
+          <TabsTrigger value="table" onClick={() => onTabChange("table")}>
             <TableIcon />
           </TabsTrigger>
         </TabsList>
       </Tabs>
     </div>
   );
-}
-
-function OverviewList({ data }) {
-	return (
-		<div className="flex flex-col space-y-4">
-			
-		</div>
-	);
 }
