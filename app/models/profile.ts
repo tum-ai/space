3685b09fa9f4bcd5b58e86ceb7b1b397c7b94830
Behsad Riemer <54889278+BehsadRiemer@users.noreply.github.com>
2024-01-20 19:27:28 +0100

    fix: profile overview (Max Von Storch) (#195)

--- app/models/profile.ts
+++ app/models/profile.ts
@@ -1,22 +1,19 @@
-export interface SocialNetwork {
-  link: string; // URL
-  type: string;
-}
-
 export interface Profile {
   id?: string;
   email: string;
   firstName: string;
   lastName: string;
-  description: string;
-  profile_picture: string;
-  socialNetworks: SocialNetwork[];
-  department: string;
-  previousDepartments: string[];
-  degree_level: string; // TODO: this should probably be it's own type
-  degree_name: string;
-  degree_semester: string;
-  currentJob: string; // TODO: why is this camel case
-  university: string;
-  nationality: string;
+  image?: string;
+
+  birthday?: Date;
+  nationality?: string;
+  description?: string;
+  activityStatus: string;
+  degreeLevel?: string;
+  degreeName?: string;
+  degreeSemester?: number;
+  university?: string;
+
+  currentDerpartment: string;
+  currentDepartmentPosition: string;
 }
