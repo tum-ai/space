5b315de0e6b25fa96206736d51e24e0590b4b5dc
behsadriemer <behsad.riemer@icloud.com>
2024-02-02 18:02:25 +0100

    Refactor: Improve error handling and Schema for Review Phases

--- app/app/api/opportunity/[opportunityId]/review/[reviewId]/reviewPhase/[phase]/route.ts
+++ app/app/api/opportunity/[opportunityId]/review/[reviewId]/reviewPhase/[phase]/route.ts
@@ -1,113 +1,129 @@
 import { NextRequest, NextResponse } from "next/server";
-import { validateReviewPhaseRequest } from "../route";
-import { updatePhoneNumber } from "firebase/auth";
+import prisma from "database/db";
 
 /**
  * GET opportunity/[opportunityId]/review/[reviewId]/reviewPhase/[phase]
  *
+ * Gets a specific ReviewPhase for a review
+ * 
  * @param NextRequest request
  * @param number opportunityId
  * @param number reviewId
  * @param string phase
- * @returns ReviewPhase reviewPhase
+ * @returns NextResponse
  */
 export const GET = async (
   request: NextRequest,
   { params: { opportunityId, reviewId, phase } },
 ): Promise<NextResponse> => {
   try {
-    const isReviewPhaseRequestValid = await validateReviewPhaseRequest(
-      parseInt(opportunityId),
-      parseInt(reviewId),
-    );
-    const readReviewPhaseByName = await prisma.phaseReview.findUnique({
+    const reviewIdNum = parseInt(reviewId);
+    if (isNaN(reviewIdNum)) {
+      return NextResponse.json({ Error: "Invalid review ID." }, { status: 400 });
+    }
+
+    const reviewPhase = await prisma.phaseReview.findUnique({
       where: {
         id: {
-          reviewId: parseInt(reviewId),
+          reviewId: reviewIdNum,
           phase: phase,
         },
       },
     });
 
-    const responseBody =
-      readReviewPhaseByName === null || !isReviewPhaseRequestValid
-        ? {
-            Error: "Bad request. The review phase with this id does not exist.",
-          }
-        : readReviewPhaseByName;
-    const status =
-      readReviewPhaseByName === null || !isReviewPhaseRequestValid ? 400 : 200;
+    if (!reviewPhase) {
+      return NextResponse.json({ Error: "Review phase does not exist." }, { status: 404 });
+    }
 
-    return NextResponse.json(responseBody, { status });
+    return NextResponse.json(reviewPhase, { status: 200 });
   } catch (error) {
-    console.log(error);
-    return NextResponse.json(
-      { Error: "Internal Server Error" },
-      { status: 500 },
-    );
+    console.error(error);
+    return NextResponse.json({ Error: "Internal Server Error" }, { status: 500 });
   }
 };
 
 /**
  * UPDATE opportunity/[opportunityId]/review/[reviewId]/reviewPhase/[phase]
  *
+ * Update a ReviewPhase of a review
+ * 
  * @param NextRequest request
  * @param number opportunityId
  * @param number reviewId
  * @param string phase
- * @returns ReviewPhase reviewPhase
+ * @param @async newPhaseName  - the new name of the phase
+ * @param @async assigneeId - the new reviewerId of this phase (refers to User ID)
+ * @returns NextResponse
  */
 export const PUT = async (
   request: NextRequest,
   { params: { opportunityId, reviewId, phase } },
 ): Promise<NextResponse> => {
   try {
-    const isReviewPhaseRequestValid = await validateReviewPhaseRequest(
-      parseInt(opportunityId),
-      parseInt(reviewId),
-    );
+    const reviewIdNum = parseInt(reviewId);
+    const { newPhaseName, assigneeId } = await request.json();
 
-    const { phaseName, assigneeId } = await request.json();
+    if (!newPhaseName || !assigneeId) {
+      return NextResponse.json({ Error: "New phase name and valid assignee ID are required." }, { status: 400 });
+    }
 
-    const updateReviewPhase = await prisma.phaseReview.update({
+    const assigneeExists = await prisma.user.findUnique({
+      where: {
+        id: assigneeId
+      },
+    });
+
+    if (!assigneeExists) {
+      return NextResponse.json({ Error: "Assignee does not exist." }, { status: 404 });
+    }
+
+    const existingPhaseReview = await prisma.phaseReview.findUnique({
       where: {
         id: {
+          reviewId: reviewIdNum,
           phase: phase,
-          reviewId: parseInt(reviewId),
+        },
+      },
+    });
+
+    if (!existingPhaseReview) {
+      return NextResponse.json({ Error: "Review phase not found." }, { status: 404 });
+    }
+
+    const updatedReviewPhase = await prisma.phaseReview.update({
+      where: {
+        id: { 
+          phase,
+          reviewId: reviewIdNum,
         },
       },
       data: {
-        phase: phaseName,
+        phase: newPhaseName,
         assigneeId: assigneeId,
       },
     });
 
-    const responseBody =
-      updateReviewPhase === null || !isReviewPhaseRequestValid
-        ? {
-            Error: "Bad request. The review phase with this id does not exist.",
-          }
-        : updateReviewPhase;
-    const status =
-      updateReviewPhase === null ||
-      !isReviewPhaseRequestValid ||
-      (phase === undefined && assigneeId === undefined)
-        ? 400
-        : 200;
-
-    return NextResponse.json(responseBody, { status });
+    return NextResponse.json(updatedReviewPhase, { status: 200 });
   } catch (error) {
-    console.log(error);
-    return NextResponse.json(
-      { Error: "Internal Server Error" },
-      { status: 500 },
-    );
+    console.error(error);
+
+    if (error.code === "P2025") {
+      return NextResponse.json({ Error: "Review phase not found." }, { status: 404 });
+    } else if (error.code === "P2002") {
+      return NextResponse.json({ Error: "Data constraint violation." }, { status: 400 });
+    }
+
+    return NextResponse.json({ Error: "Internal Server Error" }, { status: 500 });
   }
 };
+
+
 
 /**
  * DELETE opportunity/[opportunityId]/review/[reviewId]/reviewPhase/[phase]
  *
+ * Remove a ReviewPhase for a review
+ * 
  * @param NextRequest request
  * @param number opportunityId
  * @param number reviewId
@@ -119,37 +135,24 @@ export const DELETE = async (
   { params: { opportunityId, reviewId, phase } },
 ): Promise<NextResponse> => {
   try {
-    const isReviewPhaseRequestValid = await validateReviewPhaseRequest(
-      parseInt(opportunityId),
-      parseInt(reviewId),
-    );
+    const reviewIdNum = parseInt(reviewId);
 
-    const deleteReviewPhase = await prisma.phaseReview.delete({
+    await prisma.phaseReview.delete({
       where: {
         id: {
+          reviewId: reviewIdNum,
           phase: phase,
-          reviewId: parseInt(reviewId),
         },
       },
     });
 
-    const responseBody =
-      deleteReviewPhase === null || isReviewPhaseRequestValid === undefined
-        ? {
-            Error: "Bad request. The review phase with this id does not exist.",
-          }
-        : deleteReviewPhase;
-    const status =
-      deleteReviewPhase === null || isReviewPhaseRequestValid === undefined
-        ? 400
-        : 200;
-
-    return NextResponse.json(responseBody, { status });
+    return NextResponse.json({ Message: "Review phase deleted successfully." }, { status: 200 });
   } catch (error) {
-    console.log(error);
-    return NextResponse.json(
-      { Error: "Internal Server Error" },
-      { status: 500 },
-    );
+    console.error(error);
+    if (error.code === "P2025") {
+      return NextResponse.json({ Error: "Review phase not found." }, { status: 404 });
+    }
+    return NextResponse.json({ Error: "Internal Server Error" }, { status: 500 });
   }
 };
+
