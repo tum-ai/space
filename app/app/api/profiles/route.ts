a834fcdaed63da2f029ea49a478159180810617d
Amon Sisowath <78793822+Cyro292@users.noreply.github.com>
2024-02-04 20:16:03 +0100

    Add member management functionality (#197)
    Co-authored-by: Sofiia Danylchenko <sofiya.dan@gmail.com>
    Co-authored-by: behsadriemer <behsad.riemer@icloud.com>
    Co-authored-by: brebribre <70563428+brebribre@users.noreply.github.com>
    Co-authored-by: stansilav <91570355+stanislavopeida@users.noreply.github.com>

---
+++ app/app/api/profiles/route.ts
@@ -0,0 +1,95 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "database/db";
+
+
+
+export async function GET(req: NextRequest) {
+  let profiles;
+
+  try {
+    profiles = await prisma.user.findMany({
+        select: {
+            id: true,
+            firstName: true,
+            lastName: true,
+            email: true,
+            userToUserRoles: {
+              select: {
+                role: {
+                    select: {
+                        name: true,
+                    }
+                },
+              },
+            },
+            image: true,
+            departmentMemberships: {
+              select: {
+                department: {
+                  select: {
+                    id: true,
+                    name: true,
+                  },
+                },
+                departmentPosition: true,
+                membershipEnd: true,
+                membershipStart: true,
+              },
+            },
+          },
+      });
+  } catch (error) {
+      return NextResponse.json({ error: error }, { status: 400 });
+  }
+
+  const new_profiles = prepareMembershipData(profiles);
+
+  return NextResponse.json({ profiles: new_profiles }, { status: 200 });
+}
+
+function prepareMembershipData(profiles: any) {
+  // not possible to order by nested fields. function required.
+  // orderBy: {
+  //     membership_end: 'desc',
+  //     membership_start: 'desc'
+  // }
+  // in Prisma2 https://github.com/graphql-nexus/nexus-plugin-prisma/issues/458
+
+  if (!profiles) return;
+
+  if (!Array.isArray(profiles)) {
+    profiles = [profiles];
+  }
+
+  profiles.forEach((profile) => {
+    if (
+      profile.departmentMemberships &&
+      Array.isArray(profile.departmentMemberships)
+    ) {
+      profile.departmentMemberships.sort((a, b) => {
+        if (a.membershipEnd === b.membershipEnd) {
+          return b.membershipStart.getTime() - a.membershipStart.getTime();
+        }
+        return b.membershipEnd.getTime() - a.membershipEnd.getTime();
+      });
+    }
+  });
+
+  //assign new fields
+  profiles.forEach((profile) => {
+    if (
+      profile.departmentMemberships &&
+      profile.departmentMemberships?.length > 0
+    ) {
+      profile["currentDepartment"] =
+        profile.departmentMemberships[0]?.department?.name;
+      profile["currentDepartmentPosition"] =
+        profile.departmentMemberships[0]?.departmentPosition;
+    } else {
+      profile["currentDepartment"] = "";
+      profile["currentDepartmentPosition"] = "";
+    }
+  });
+
+  return profiles;
+}
---
+++ app/app/members/components/DataTableToolbar.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+import { Cross2Icon } from "@radix-ui/react-icons";
+import { Table } from "@tanstack/react-table";
+
+import { Button } from "@components/ui/button";
+import { Input } from "@components/ui/input";
+import { DataTableViewOptions } from "./DataTableViewOptions";
+
+import { DataTableFacetedFilter } from "./DataTableFacetedFilter";
+import React, { useState, useEffect } from "react";
+import {
+  getRolesMap,
+  getPositionsMap,
+  getDepartmentsMap,
+} from "@lib/retrievals";
+import DataTableEditDialog from "./DataTableEditDialog";
+
+interface DataTableToolbarProps<TData> {
+  table: Table<TData>;
+  tableData: object;
+}
+
+export function DataTableToolbar<TData>({
+  table,
+  tableData
+}: DataTableToolbarProps<TData>) {
+  const isFiltered = table.getState().columnFilters.length > 0;
+  const [departments, setDepartments] = useState([]);
+  const [roles, setRoles] = useState([]);
+  const [positions, setPositions] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const departments = await getDepartmentsMap();
+        const roles = await getRolesMap();
+        const positions = await getPositionsMap();
+        setDepartments(departments);
+        setRoles(roles);
+        setPositions(positions);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const getSelectedRows = () => {
+    const tmp = [];
+    table
+      .getRowModel()
+      .rows.filter((row) => row.getIsSelected())
+      .map((row) => {
+        tmp.push(row._valuesCache);
+      });
+    return tmp;
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-between gap-2 sm:flex-row">
+      <div
+        className="flex flex-1 flex-wrap items-center gap-2"
+        style={{ maxWidth: "100%" }}
+      >
+        <Input
+          placeholder={`Filter ${table.getColumn("email")?.columnDef[
+            "label"
+          ]}...`}
+          value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("email")?.setFilterValue(event.target.value)
+          }
+          className="h-8 w-full sm:w-[150px] lg:w-[250px]"
+        />
+        <Input
+          placeholder={`Filter ${table.getColumn("lastName")?.columnDef[
+            "label"
+          ]}...`}
+          value={
+            (table.getColumn("lastName")?.getFilterValue() as string) ?? ""
+          }
+          onChange={(event) =>
+            table.getColumn("lastName")?.setFilterValue(event.target.value)
+          }
+          className="h-8 w-full sm:w-[150px] lg:w-[250px]"
+        />
+        {table.getColumn("userRoles") && (
+          <DataTableFacetedFilter
+            column={table.getColumn("userRoles")}
+            title={table.getColumn("userRoles").columnDef["label"]}
+            options={roles}
+          />
+        )}
+        {table.getColumn("currentDepartment") && (
+          <DataTableFacetedFilter
+            column={table.getColumn("currentDepartment")}
+            title={table.getColumn("currentDepartment").columnDef["label"]}
+            options={departments}
+          />
+        )}
+        {table.getColumn("currentDepartmentPosition") && (
+          <DataTableFacetedFilter
+            column={table.getColumn("currentDepartmentPosition")}
+            title={
+              table.getColumn("currentDepartmentPosition").columnDef["label"]
+            }
+            options={positions}
+          />
+        )}
+        {isFiltered && (
+          <Button
+            variant="ghost"
+            onClick={() => table.resetColumnFilters()}
+            className="h-8 px-2 lg:px-3"
+          >
+            Reset
+            <Cross2Icon className="ml-2 h-4 w-4" />
+          </Button>
+        )}
+      </div>
+      <DataTableEditDialog tableData={tableData} rows={getSelectedRows()}/>
+      <DataTableViewOptions table={table} />
+    </div>
+  );
+}
