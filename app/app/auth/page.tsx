6ddc3aa304d3aa9ad569d7dc1ffa3c41cfbc6893
simonhng <simon.huang@tum.de>
2024-02-06 21:35:09 +0100

    update ux for auth pages

--- app/app/auth/page.tsx
+++ app/app/auth/page.tsx
@@ -1,16 +1,34 @@
 "use client";
-import { useState } from "react";
+import { Section } from "@components/Section";
 import { LoginForm } from "./components/LoginForm";
-import { PasswordReset } from "./components/PasswordReset";
+import { env } from "env.mjs";
+import { Button } from "@components/ui/button";
+import { signIn } from "next-auth/react";
+import { SlackLogo } from "@phosphor-icons/react";
+import Link from "next/link";
 
 const Auth = () => {
-  const [isResetPassword, setResetPassword] = useState(false);
+  const isDevelopment = env.NEXT_PUBLIC_VERCEL_ENV === "development";
 
   return (
-    <>
-      {!isResetPassword && <LoginForm setResetPassword={setResetPassword} />}
-      {isResetPassword && <PasswordReset setResetPassword={setResetPassword} />}
-    </>
+    <Section>
+      <div className="mx-auto max-w-lg space-y-8">
+        {isDevelopment && <LoginForm />}
+        {!isDevelopment && (
+          <Button className="w-full" onClick={() => signIn("slack")}>
+            <SlackLogo size={20} className="mr-2" /> Log in with Slack
+          </Button>
+        )}
+        {isDevelopment && (
+          <p className="w-full text-center">
+            Don&apos;t have an account?{" "}
+            <Link href="auth/register" className="underline">
+              Register
+            </Link>
+          </p>
+        )}
+      </div>
+    </Section>
   );
 };
 
