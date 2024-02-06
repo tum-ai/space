e6b07b7b2df88591cf95bd10f2ad9aba4ccb74fd
simonhng <simon.huang@tum.de>
2024-02-06 20:50:07 +0100

    remove unused routes

--- app/components/NavBar/index.tsx
+++ app/components/NavBar/index.tsx
@@ -1,49 +1,26 @@
-import ProtectedItem from "@components/ProtectedItem";
 import { Section } from "@components/Section";
 import * as NavigationMenu from "@radix-ui/react-navigation-menu";
-import clsx from "clsx";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Logo from "./components/Logo";
 import User from "./components/User";
 import { Button } from "@components/ui/button";
+import { cn } from "@lib/utils";
+
+interface Link {
+  href: string;
+  text: string;
+}
 
 function NavBar() {
   const pathname = usePathname();
-  const links: {
-    href: string;
-    text: string;
-    protected?: boolean;
-    roles?: string[];
-  }[] = [
+  const links: Link[] = [
     {
       href: "/profiles/",
-      text: "Team",
-    },
-    {
-      href: "/invite/",
-      text: "Invite Members",
-      protected: true,
-      roles: ["invite_members"],
-    },
-    {
-      href: "/certificate/",
-      text: "Certificate",
-      protected: true,
-      roles: ["create_certificate"],
-    },
-    {
-      href: "/review/",
-      text: "Review Tool",
-      protected: true,
-      roles: ["submit_reviews"],
-    },
-    {
-      href: "/referrals/",
-      text: "Referrals",
-      protected: true,
+      text: "Profiles",
     },
   ];
+
   return (
     <NavigationMenu.Root className="relative w-full">
       <Section>
@@ -60,7 +37,7 @@ function NavBar() {
                     <Button
                       asChild
                       variant="link"
-                      className={clsx(
+                      className={cn(
                         "w-max hover:text-black dark:hover:text-white",
                         link.href === pathname
                           ? "text-black dark:text-white"
@@ -72,14 +49,6 @@ function NavBar() {
                   </NavigationMenu.Link>
                 </NavigationMenu.Item>
               );
-
-              if (link.protected) {
-                return (
-                  <ProtectedItem key={link.href} roles={link.roles}>
-                    <Component />
-                  </ProtectedItem>
-                );
-              }
 
               return (
                 <span key={link.href}>
