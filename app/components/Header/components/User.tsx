e7a73e9c9f1ac04440c36e1a6ecc92dc2252ecf2
simonhng <simon.huang@tum.de>
2024-02-25 14:37:37 +0100

    add basic navigation

--- app/components/Header/components/User.tsx
+++ app/components/Header/components/User.tsx
@@ -1,4 +1,4 @@
-import { Avatar } from "@components/Avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@components/ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -8,17 +8,13 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@components/ui/dropdown-menu";
-import { EnterIcon } from "@radix-ui/react-icons";
 import { authOptions } from "app/api/auth/[...nextauth]/route";
 import { LogOut, User as UserIcon } from "lucide-react";
-import { Session, getServerSession } from "next-auth";
-import { signOut } from "next-auth/react";
+import { getServerSession } from "next-auth";
 import Link from "next/link";
 
-interface LoggedInUserProps {
-  user: Session["user"];
-}
-const LoggedInUser = ({ user }: LoggedInUserProps) => {
+export const User = async () => {
+  const { user } = await getServerSession(authOptions);
   return (
     <DropdownMenu>
       <DropdownMenuTrigger
@@ -26,11 +22,12 @@ const LoggedInUser = ({ user }: LoggedInUserProps) => {
           "flex items-center space-x-4 rounded-full hover:text-black dark:bg-gray-700 dark:hover:text-white sm:bg-gray-100 sm:p-2 sm:px-4"
         }
       >
-        <Avatar
-          variant="circle"
-          profilePicture={user.image}
-          initials={`${user.firstName[0]}${user.lastName[0]}`}
-        />
+        <Avatar>
+          <AvatarImage src={user.image} />
+          <AvatarFallback>
+            {`${user.firstName[0]}${user.lastName[0]}`}
+          </AvatarFallback>
+        </Avatar>
         <p className="hidden sm:flex">
           {user.firstName} {user.lastName}
         </p>
@@ -41,38 +38,19 @@ const LoggedInUser = ({ user }: LoggedInUserProps) => {
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
           <DropdownMenuItem asChild>
-            <Link href="/me">
+            <Link href="/profile">
               <UserIcon className="mr-2 h-4 w-4" />
               <span>Profile</span>
             </Link>
           </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => signOut({ redirect: false })}>
-            <LogOut className="mr-2 h-4 w-4" />
-            <span>Log out</span>
+          <DropdownMenuItem asChild>
+            <Link href="/auth/signout">
+              <LogOut className="mr-2 h-4 w-4" />
+              <span>Log out</span>
+            </Link>
           </DropdownMenuItem>
         </DropdownMenuGroup>
       </DropdownMenuContent>
     </DropdownMenu>
   );
 };
-
-async function User() {
-  return (
-    <div className="flex space-x-4">
-      {/*
-      {session.user && <LoggedInUser user={session.user} />}
-
-      {!session.user && (
-        <Link
-          href={"/auth"}
-          className="flex items-center gap-2 rounded-lg bg-white p-2 dark:bg-gray-700"
-        >
-          <EnterIcon /> Login
-        </Link>
-      )}
-      */}
-    </div>
-  );
-}
-
-export default User;
--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,10 +1,21 @@
 import { authOptions } from "app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
+import { Navigation } from "./components/navigation";
+import { User } from "./components/user";
+import Link from "next/link";
 
 async function Header() {
   const session = await getServerSession(authOptions);
 
-  return <header>{session.user.firstName}</header>;
+  return (
+    <header className="w-full">
+      <div className="flex w-full justify-between px-2 py-4">
+        <Navigation />
+        {session.user && <User />}
+        {!session.user && <Link href="/auth">Sign in</Link>}
+      </div>
+    </header>
+  );
 }
 
 export default Header;
