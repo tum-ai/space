38651bc76b7c30de11311b216f4f60aeaab0845d
simonhng <simon.huang@tum.de>
2024-02-25 16:27:16 +0100

    make headlines consistent

--- app/app/members/page.tsx
+++ app/app/members/page.tsx
@@ -1,7 +1,6 @@
 import { Metadata } from "next";
 
 import { DataTable } from "./components/DataTable";
-import { Section } from "@components/Section";
 
 export const metadata: Metadata = {
   title: "Members",
@@ -10,22 +9,16 @@ export const metadata: Metadata = {
 
 export default function MembersPage() {
   return (
-    <>
-      <Section>
-        <div className="h-full flex-1 flex-col space-y-8 p-8 md:flex">
-          <div className="flex items-center justify-between space-y-2">
-            <div>
-              <h2 className="text-2xl font-bold tracking-tight">
-                Welcome back!
-              </h2>
-              <p className="text-muted-foreground">
-                Here&apos;s a list of all members.
-              </p>
-            </div>
-          </div>
-          <DataTable></DataTable>
+    <div className="h-full flex-1 flex-col space-y-8 p-8 md:flex">
+      <div className="flex items-center justify-between space-y-2">
+        <div>
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            Members list
+          </h1>
+          <p className="text-muted-foreground">Manage and list members</p>
         </div>
-      </Section>
-    </>
+      </div>
+      <DataTable></DataTable>
+    </div>
   );
 }
