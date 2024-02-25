b3dac65ceeecba845348c1a90b60288932b5f110
simonhng <simon.huang@tum.de>
2024-02-25 16:09:40 +0100

    remove unused files

--- app/app/not-found.tsx
+++ app/app/not-found.tsx
@@ -1,14 +1,15 @@
 import { Button } from "@components/ui/button";
-import { Section } from "@components/Section";
 import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <Section className="flex flex-col items-center justify-center gap-4">
-      <h1 className="text-4xl">Ups, looks like you are lost..</h1>
-      <Button asChild>
-        <Link href={"/"}>return home</Link>
-      </Button>
-    </Section>
+    <section className="flex flex-col items-center justify-center gap-4">
+      <h1 className="text-4xl">Looks like you are lost..</h1>
+      <Link href={"/"}>
+        <Button variant="link" className="flex gap-2">
+          Return home
+        </Button>
+      </Link>
+    </section>
   );
 }
