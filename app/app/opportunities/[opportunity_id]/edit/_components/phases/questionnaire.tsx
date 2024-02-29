74ab3ccc7168345580204e726813d8f6a6a757be
simonhng <simon.huang@tum.de>
2024-02-29 14:58:56 +0100

    restructure creation of questionnaires

---
+++ app/app/opportunities/[opportunity_id]/edit/_components/phases/questionnaire.tsx
@@ -0,0 +1,177 @@
+import {
+  SubmitHandler,
+  UseFieldArrayAppend,
+  UseFormProps,
+  useFieldArray,
+  useForm,
+} from "react-hook-form";
+import { z } from "zod";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@components/ui/dialog";
+import {
+  OpportunitySchema,
+  QuestionnaireSchema,
+} from "@lib/schemas/opportunity";
+import { Button } from "@components/ui/button";
+import { QuestionForm } from "../question";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@components/ui/form";
+import { Input } from "@components/ui/input";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FileMinus, FilePlus2, Plus, Save, UserPlus } from "lucide-react";
+
+interface QuestionnaireProps {
+  append: UseFieldArrayAppend<
+    z.infer<typeof OpportunitySchema>,
+    `defineSteps.${number}.forms`
+  >;
+  onRemove: () => void;
+  defaultValues: UseFormProps<
+    z.infer<typeof QuestionnaireSchema>
+  >["defaultValues"];
+}
+
+export const Questionnaire = ({
+  append,
+  onRemove: removeQuestionnaire,
+  defaultValues,
+}: QuestionnaireProps) => {
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const form = useForm<z.infer<typeof QuestionnaireSchema>>({
+    resolver: zodResolver(QuestionnaireSchema),
+    defaultValues,
+  });
+
+  const onSubmit: SubmitHandler<z.infer<typeof QuestionnaireSchema>> = (
+    data,
+  ) => {
+    append(data);
+    setDialogOpen(false);
+  };
+
+  const {
+    fields: questions,
+    append: appendQuestion,
+    remove: removeQuestion,
+    update: updateQuestion,
+  } = useFieldArray({
+    control: form.control,
+    name: `questions`,
+  });
+
+  const {
+    fields: reviewers,
+    append: appendReviewer,
+    remove: removeReviewer,
+    update: updateReviewer,
+  } = useFieldArray({
+    control: form.control,
+    name: `reviewers`,
+  });
+
+  return (
+    <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+      <Button asChild variant="outline" className="w-full">
+        <DialogTrigger>{form.watch("name")}</DialogTrigger>
+      </Button>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Edit questionaire</DialogTitle>
+          <DialogDescription>
+            Configure questions and reviewer for {form.watch("name")}
+          </DialogDescription>
+        </DialogHeader>
+
+        <FormField
+          control={form.control}
+          name={`name`}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name*</FormLabel>
+              <FormControl>
+                <Input placeholder="General" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <div className="mt-8">
+          <h3 className="mb-4 scroll-m-20 text-2xl font-semibold tracking-tight">
+            Questions
+          </h3>
+          <div className="space-y-4">
+            {questions.map((question, index) => (
+              <QuestionForm
+                key={question.id + index}
+                question={question}
+                index={index}
+                update={updateQuestion}
+                remove={removeQuestion}
+              />
+            ))}
+
+            <Button
+              className="w-full"
+              variant="secondary"
+              type="button"
+              onClick={() => appendQuestion({ type: "text", question: "" })}
+            >
+              <FilePlus2 className="mr-2" />
+              Add question
+            </Button>
+          </div>
+        </div>
+
+        <div className="mt-8">
+          <h3 className="mb-4 scroll-m-20 text-2xl font-semibold tracking-tight">
+            Reviewer
+          </h3>
+          <div className="space-y-4">
+            {reviewers.map((reviewer) => (
+              <>{reviewer.name}</>
+            ))}
+
+            <Button className="w-full" variant="secondary" type="button">
+              <UserPlus className="mr-2" />
+              Add reviewer
+            </Button>
+          </div>
+        </div>
+
+        <DialogFooter>
+          <Button
+            type="button"
+            variant="destructive"
+            onClick={removeQuestionnaire}
+          >
+            <FileMinus className="mr-2" />
+            Remove
+          </Button>
+          <Button
+            type="button"
+            onClick={(event) => {
+              void form.handleSubmit(onSubmit, console.error)(event);
+            }}
+          >
+            <Save className="mr-2" />
+            Save
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
