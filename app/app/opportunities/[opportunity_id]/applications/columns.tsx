ef75c0931e66e302f7d95dabc022e615b0182862
Simon Huang <simon.huang@tum.de>
2024-03-02 08:50:05 +0100

    feat: add application overview (#215)
    Co-authored-by: Nils Rehtanz <nils.rehtanz@hotmail.de>

---
+++ app/app/opportunities/[opportunity_id]/applications/columns.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { Application } from "@prisma/client";
+import { Button } from "@components/ui/button";
+import { Edit } from "lucide-react";
+import Link from "next/link";
+
+export const columns: ColumnDef<Application>[] = [
+  {
+    accessorKey: "id",
+    header: () => "ID",
+  },
+  {
+    accessorKey: "createdAt",
+    header: () => "Created At",
+  },
+  {
+    id: "actions",
+    header: "edit",
+    cell: ({ row }) => {
+      const application = row.original;
+
+      return (
+        <Button size="icon" asChild>
+          <Link href={`applications/${application.id}`}>
+            <Edit />
+          </Link>
+        </Button>
+      );
+    },
+  },
+];
