fce04505806ea2a793c5e90fbc3abcfa8a5a7890
Simon Huang <simon.huang@tum.de>
2024-03-02 08:20:28 +0100

    feat: add review overview page (#214)

--- app/app/members/page.tsx
+++ app/app/members/page.tsx
@@ -9,14 +9,12 @@ export const metadata: Metadata = {
 
 export default function MembersPage() {
   return (
-    <div className="h-full flex-1 flex-col space-y-8 p-8 md:flex">
-      <div className="flex items-center justify-between space-y-2">
-        <div>
-          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-            Members list
-          </h1>
-          <p className="text-muted-foreground">Manage and list members</p>
-        </div>
+    <div className="space-y-8 p-8">
+      <div>
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Members list
+        </h1>
+        <p className="text-muted-foreground">Manage and list members</p>
       </div>
       <DataTable></DataTable>
     </div>
