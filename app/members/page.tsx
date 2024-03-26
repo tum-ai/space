8df39d33e6a7aac64e9f08f1dd88c149f0e70a75
Cyro292 <amon@archi.de>
2024-03-26 14:40:06 +0100

    feat: Add members page

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,11 +1,63 @@
-import { Metadata } from "next";
+import { DataTable } from "./components/DataTable";
+import { RowUser } from "./components/DataTableTypes";
+import db from "server/db";
+import { DepartmentRole, SpaceRole } from "@prisma/client";
+import { getServerAuthSession } from "server/auth";
+import { redirect } from "next/navigation";
 
-export const metadata: Metadata = {
-  title: "Members",
-  description: "A task and issue tracker build using Tanstack Table.",
-};
+export default async function MembersPage() {
+  const session = await getServerAuthSession();
+  const userId = session?.user.id;
+  if (!userId) redirect("/auth");
 
-export default function MembersPage() {
+  const users = await db.user.findMany({
+    include: {
+      departmentMemberships: {
+        include: {
+          department: true,
+        },
+      },
+    },
+  });
+
+  const profiles: RowUser[] = users.map((user) => {
+    return {
+      id: user.id,
+      name: user.name ?? "",
+      email: user.email,
+      roles: user.roles,
+      image: user.image ?? undefined,
+      currentDepartment: user.departmentMemberships[0]?.department?.name ?? "",
+      currentDepartmentPosition: user.departmentMemberships[0]?.role ?? "",
+    };
+  });
+
+  const departments = await db.department.findMany().then((departments) => {
+    return departments.map((department) => ({
+      label: department.name,
+      value: department.name,
+    }));
+  });
+
+  const roles = Object.entries(SpaceRole).map((role) => ({
+    label:
+      role[1].charAt(0) + role[1].slice(1).toLowerCase().replaceAll("_", " "),
+    value: role[1],
+  }));
+
+  const positions = Object.entries(DepartmentRole).map((position) => ({
+    label:
+      position[1].charAt(0) +
+      position[1].slice(1).toLowerCase().replaceAll("_", " "),
+    value: position[1],
+  }));
+
+  const columnData = {
+    departments: departments,
+    roles: roles,
+    positions: positions,
+  };
+
   return (
     <div className="space-y-8 p-8">
       <div>
@@ -14,6 +66,7 @@ export default function MembersPage() {
         </h1>
         <p className="text-muted-foreground">Manage and list members</p>
       </div>
+      <DataTable rowData={profiles} columnData={columnData}></DataTable>
     </div>
   );
 }
