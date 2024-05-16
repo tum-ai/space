dbbaa057077a1181808aee446b24797b18fe693d
Max-vS <max@vonstorch.com>
2024-05-16 16:07:18 +0200

    revert route changes

--- server/api/routers/user.ts
+++ server/api/routers/user.ts
@@ -1,20 +1,6 @@
-import { UserSchema } from "@lib/schemas/user";
 import { createTRPCRouter, protectedProcedure } from "server/api/trpc";
 
 export const userRouter = createTRPCRouter({
-  create: protectedProcedure
-    .input(UserSchema)
-    .mutation(async ({ input, ctx }) => {
-      const user = await ctx.db.user.create({
-        data: {
-          email: input.email,
-          name: input.name,
-          image: input.image,
-        },
-      });
-      return user;
-    }),
-
   getAll: protectedProcedure.query(async ({ ctx }) => {
     try {
       const res = await ctx.db.user.findMany();
