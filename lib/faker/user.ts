8f79cb71bfafe203b815289b138f148e9632525f
Max-vS <117863553+Max-vS@users.noreply.github.com>
2024-06-08 13:54:58 +0200

    feat: faker applications and reviews (#240)

---
+++ lib/faker/user.ts
@@ -0,0 +1,28 @@
+import { Person } from "@lib/types/person";
+import db from "server/db";
+import { emailUniqueEnforcer } from "./utils";
+import { faker } from "@faker-js/faker";
+import { v4 as uuidv4 } from "uuid";
+
+export async function generateUsers(number: number): Promise<Person[]> {
+	const generatedUsers = Array.from({ length: number }, () => generateUser());
+	await db.user.createMany({
+	  data: generatedUsers,
+	});
+	const users = generatedUsers.map((u) => ({
+	  id: u.id,
+	  name: u.name!,
+	  image: u.image!,
+	}));
+	return users;
+  }
+  
+  function generateUser() {
+	return {
+	  id: uuidv4(),
+	  email: emailUniqueEnforcer.enforce(() => faker.internet.exampleEmail()),
+	  name: faker.person.fullName(),
+	  image: faker.image.avatar(),
+	};
+  }
+
--- lib/faker/utils.ts
+++ lib/faker/utils.ts
@@ -1,5 +1,6 @@
 import { UniqueEnforcer } from "enforce-unique";
 import { Command } from "commander";
+import db from "server/db";
 
 // Unique Enforcers
 export const emailUniqueEnforcer = new UniqueEnforcer();
@@ -24,19 +25,37 @@ program
   .option(
     "-maxp, --maxphases <number>",
     "maximal number of phases to generate per opportunity",
-    "5",
+    "4",
   )
   .option(
     "-minq, --minquestionnaires <number>",
     "minimal number of questionnaires to generate per phase",
-    "2",
+    "1",
   )
   .option(
     "-maxq, --maxquestionnaires <number>",
     "maximal number of questionnaires to generate per phase",
-    "5",
+    "3",
+  ).option(
+    "-mina, --minapplications <number>",
+    "minimal number of applications to generate per opportunity",
+    "1"
+  )
+  .option(
+    "-maxa, --maxapplications <number>",
+    "maximal number of applications to generate per opportunity",
+    "5"
   )
   .parse(process.argv);
 
 export const options = program.opts();
 export const now = new Date();
+
+export async function fetchAllUsers() {
+  try {
+    const users = await db.user.findMany();
+    return users;
+  } catch (error) {
+    console.error("Error fetching users:", error);
+  }
+}
