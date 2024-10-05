774c6fc0fc34a70de1c2fcf76c0e362c9329cd9d
Simon Huang <simonhng@Bofans-MacBook-Air.local>
2024-10-05 21:38:37 +0200

    add leaderboard

---
+++ app/opportunities/[opportunity_id]/leaderboard/page.tsx
@@ -0,0 +1,67 @@
+import { redirect } from "next/navigation";
+import { getServerAuthSession } from "server/auth";
+import db from "server/db";
+import { Leaderboard } from "../_components/Leaderboard";
+import Breadcrumbs from "@components/ui/breadcrumbs";
+
+interface OpportunityProps {
+  params: { opportunity_id: string };
+}
+
+export default async function OpportunityPage({ params }: OpportunityProps) {
+  const session = await getServerAuthSession();
+  if (!session?.user?.id) redirect("/auth");
+
+  const opportunity = await db.opportunity.findUnique({
+    where: {
+      id: Number(params.opportunity_id),
+    },
+  });
+
+  const reviewer = await db.user.findMany({
+    where: {
+      reviews: {
+        some: {
+          application: {
+            opportunityId: Number(params.opportunity_id),
+          },
+        },
+      },
+    },
+    select: {
+      id: true,
+      name: true,
+      _count: {
+        select: {
+          reviews: true,
+        },
+      },
+    },
+  });
+
+  return (
+    <div className="space-y-8 p-8">
+      <div className="flex justify-between">
+        <div className="flex flex-col gap-3">
+          <Breadcrumbs
+            title={"Leaderboard"}
+            opportunityTitle={opportunity?.title}
+          />
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            Leaderboard
+          </h1>
+          <p className="text-muted-foreground">
+            See who is carrying this student initiative
+          </p>
+        </div>
+      </div>
+
+      <Leaderboard
+        reviewers={reviewer.map((reviewer) => ({
+          name: reviewer.name ?? reviewer.id,
+          applicationsReviewed: reviewer._count.reviews,
+        }))}
+      />
+    </div>
+  );
+}
